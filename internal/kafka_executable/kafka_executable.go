e95e4c2fa1c9c7d8a589e2cc635f225d41cd4b1d
Ryan Gang <ryan-gg@outlook.com>
2024-08-28 16:26:20 +0530

    feat: add kafka_executable

---
+++ internal/kafka_executable/kafka_executable.go
@@ -0,0 +1,67 @@
+package kafka_executable
+
+import (
+	"fmt"
+	"path"
+	"strings"
+
+	executable "github.com/codecrafters-io/tester-utils/executable"
+	logger "github.com/codecrafters-io/tester-utils/logger"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+type KafkaExecutable struct {
+	executable *executable.Executable
+	logger     *logger.Logger
+	args       []string
+}
+
+func NewKafkaExecutable(stageHarness *test_case_harness.TestCaseHarness) *KafkaExecutable {
+	b := &KafkaExecutable{
+		executable: stageHarness.NewExecutable(),
+		logger:     stageHarness.Logger,
+	}
+
+	stageHarness.RegisterTeardownFunc(func() { b.Kill() })
+
+	return b
+}
+
+func (b *KafkaExecutable) Run(args ...string) error {
+	b.args = args
+	if b.args == nil || len(b.args) == 0 {
+		b.logger.Infof("$ ./%s", path.Base(b.executable.Path))
+	} else {
+		var log string
+		log += fmt.Sprintf("$ ./%s", path.Base(b.executable.Path))
+		for _, arg := range b.args {
+			if strings.Contains(arg, " ") {
+				log += " \"" + arg + "\""
+			} else {
+				log += " " + arg
+			}
+		}
+		b.logger.Infof(log)
+	}
+
+	if err := b.executable.Start(b.args...); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *KafkaExecutable) HasExited() bool {
+	return b.executable.HasExited()
+}
+
+func (b *KafkaExecutable) Kill() error {
+	b.logger.Debugf("Terminating program")
+	if err := b.executable.Kill(); err != nil {
+		b.logger.Debugf("Error terminating program: '%v'", err)
+		return err
+	}
+
+	b.logger.Debugf("Program terminated successfully")
+	return nil // When does this happen?
+}
