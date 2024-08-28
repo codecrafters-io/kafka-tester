5ac4766e9741d700ebff4d1ff9de5db0aa5c6e08
Ryan Gang <ryan-gg@outlook.com>
2024-08-28 16:26:53 +0530

    feat: add stage1 bind test case

--- internal/stage1.go
+++ internal/stage1.go
@@ -1,23 +1,23 @@
 package internal
 
 import (
-	"github.com/codecrafters-io/kafka-tester/protocol"
-	kafkaapi "github.com/codecrafters-io/kafka-tester/protocol/api"
+	"github.com/codecrafters-io/kafka-tester/internal/kafka_executable"
+	"github.com/codecrafters-io/kafka-tester/internal/test_cases"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
-func GetAPIVersions(prettyPrint bool) {
-	broker := protocol.NewBroker("localhost:9092")
-	if err := broker.Connect(); err != nil {
-		panic(err)
-	}
-	defer broker.Close()
-
-	response, err := kafkaapi.ApiVersions(broker, &kafkaapi.ApiVersionsRequest{Version: 3, ClientSoftwareName: "kafka-cli", ClientSoftwareVersion: "0.1"})
-	if err != nil {
-		panic(err)
+func testBindToPort(stageHarness *test_case_harness.TestCaseHarness) error {
+	b := kafka_executable.NewKafkaExecutable(stageHarness)
+	if err := b.Run(); err != nil {
+		return err
 	}
 
-	if prettyPrint {
-		kafkaapi.PrintAPIVersions(response)
+	logger := stageHarness.Logger
+
+	bindTestCase := test_cases.BindTestCase{
+		Port:    9093,
+		Retries: 15,
 	}
+
+	return bindTestCase.Run(b, logger)
 }
