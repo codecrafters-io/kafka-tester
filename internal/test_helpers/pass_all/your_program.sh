2942d9847fab507c22ceaac6070dd77fa09beb95
Ryan Gang <ryan-gg@outlook.com>
2024-08-30 11:16:59 +0530

    chore: silence all logs from kafka broker

--- internal/test_helpers/pass_all/your_program.sh
+++ internal/test_helpers/pass_all/your_program.sh
@@ -5,4 +5,4 @@ rm -rf /tmp/kafka-logs /tmp/zookeeper /tmp/kraft-combined-logs
 cd /Users/ryang/Developer/tmp/kafka/kafka_2.13-3.8.0
 KAFKA_CLUSTER_ID="$(./bin/kafka-storage.sh random-uuid)"
 ./bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/kraft/server.properties
-./bin/kafka-server-start.sh config/kraft/server.properties
+./bin/kafka-server-start.sh config/kraft/server.properties > /dev/null 2>&1
