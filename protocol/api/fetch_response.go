b4840cd5485d859955ebce26a7e96fde1393a2f9
Ryan Gang <ryan-gg@outlook.com>
2024-09-04 14:32:48 +0530

    refactor: add contextual error messages in fetch response decoding

--- protocol/api/fetch_response.go
+++ protocol/api/fetch_response.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/codecrafters-io/kafka-tester/protocol/decoder"
 	"github.com/codecrafters-io/kafka-tester/protocol/encoder"
+	"github.com/codecrafters-io/kafka-tester/protocol/errors"
 )
 
 type FetchResponse struct {
@@ -19,20 +20,32 @@ func (r *FetchResponse) Decode(pd *decoder.RealDecoder, version int16) (err erro
 	r.Version = version
 
 	if r.ThrottleTimeMs, err = pd.GetInt32(); err != nil {
-		return fmt.Errorf("failed to decode throttleTimeMs in fetch response: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("throttleTimeMs").WithAddedContext("FetchResponse")
+		}
+		return err
 	}
 
 	if r.ErrorCode, err = pd.GetInt16(); err != nil {
-		return fmt.Errorf("failed to decode errorCode in fetch response: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("errorCode").WithAddedContext("FetchResponse")
+		}
+		return err
 	}
 
 	if r.SessionID, err = pd.GetInt32(); err != nil {
-		return fmt.Errorf("failed to decode sessionID in fetch response: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("sessionID").WithAddedContext("FetchResponse")
+		}
+		return err
 	}
 
 	numResponses, err := pd.GetCompactArrayLength()
 	if err != nil {
-		return fmt.Errorf("failed to decode numResponses in fetch response: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("numResponses").WithAddedContext("FetchResponse")
+		}
+		return err
 	}
 
 	r.Responses = make([]TopicResponse, numResponses)
@@ -40,14 +53,23 @@ func (r *FetchResponse) Decode(pd *decoder.RealDecoder, version int16) (err erro
 		topicResponse := TopicResponse{}
 		err := topicResponse.Decode(pd)
 		if err != nil {
-			return fmt.Errorf("failed to decode topic response in fetch response: %w", err)
+			if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+				return decodingErr.WithAddedContext(fmt.Sprintf("topicResponse[%d]", i)).WithAddedContext("FetchResponse")
+			}
+			return err
 		}
 		r.Responses[i] = topicResponse
 	}
-	pd.GetEmptyTaggedFieldArray()
+	_, err = pd.GetEmptyTaggedFieldArray()
+	if err != nil {
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("taggedFieldArray").WithAddedContext("FetchResponse")
+		}
+		return err
+	}
 
 	if pd.Remaining() != 0 {
-		return fmt.Errorf("remaining bytes in decoder: %d", pd.Remaining())
+		return errors.NewPacketDecodingError(fmt.Sprintf("unexpected %d bytes remaining in decoder after decoding FetchResponse", pd.Remaining()), "FetchResponse")
 	}
 
 	return nil
@@ -61,30 +83,49 @@ type TopicResponse struct {
 func (tr *TopicResponse) Decode(pd *decoder.RealDecoder) (err error) {
 	topicUUIDBytes, err := pd.GetRawBytes(16)
 	if err != nil {
-		return fmt.Errorf("failed to decode topicUUID in topic response: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("topicUUIDBytes")
+		}
+		return err
 	}
 	topicUUID, err := encoder.DecodeUUID(topicUUIDBytes)
 	if err != nil {
-		return fmt.Errorf("failed to decode in func: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("topicUUID").WithAddedContext("topicResponse")
+		}
+		return err
 	}
 	tr.Topic = topicUUID
 
 	numPartitions, err := pd.GetCompactArrayLength()
 	if err != nil {
-		return fmt.Errorf("failed to decode in func: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("numPartitions")
+		}
+		return err
 	}
 	tr.Partitions = make([]PartitionResponse, numPartitions)
-	// partition := &tr.Partitions[j]
 
 	for j := range tr.Partitions {
 		partition := PartitionResponse{}
 		err := partition.Decode(pd)
 		if err != nil {
-			return fmt.Errorf("failed to decode partition response in topic response: %w", err)
+			if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+				return decodingErr.WithAddedContext(fmt.Sprintf("partitionResponse[%d]", j))
+			}
+			return err
 		}
 		tr.Partitions[j] = partition
 	}
-	pd.GetEmptyTaggedFieldArray()
+	// Add errors for all the EmptyTaggedFieldArray calls
+	// ToDo
+	_, err = pd.GetEmptyTaggedFieldArray()
+	if err != nil {
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("taggedFieldArray")
+		}
+		return err
+	}
 
 	return nil
 }
@@ -102,28 +143,46 @@ type PartitionResponse struct {
 
 func (pr *PartitionResponse) Decode(pd *decoder.RealDecoder) (err error) {
 	if pr.PartitionIndex, err = pd.GetInt32(); err != nil {
-		return fmt.Errorf("failed to decode in func: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("partitionIndex")
+		}
+		return err
 	}
 
 	if pr.ErrorCode, err = pd.GetInt16(); err != nil {
-		return fmt.Errorf("failed to decode in func: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("errorCode")
+		}
+		return err
 	}
 
 	if pr.HighWatermark, err = pd.GetInt64(); err != nil {
-		return fmt.Errorf("failed to decode in func: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("highWatermark")
+		}
+		return err
 	}
 
 	if pr.LastStableOffset, err = pd.GetInt64(); err != nil {
-		return fmt.Errorf("failed to decode in func: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("lastStableOffset")
+		}
+		return err
 	}
 
 	if pr.LogStartOffset, err = pd.GetInt64(); err != nil {
-		return fmt.Errorf("failed to decode in func: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("logStartOffset")
+		}
+		return err
 	}
 
 	numAbortedTransactions, err := pd.GetCompactArrayLength()
 	if err != nil {
-		return fmt.Errorf("failed to decode in func: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("numAbortedTransactions")
+		}
+		return err
 	}
 
 	if numAbortedTransactions > 0 {
@@ -132,32 +191,52 @@ func (pr *PartitionResponse) Decode(pd *decoder.RealDecoder) (err error) {
 			abortedTransaction := AbortedTransaction{}
 			err := abortedTransaction.Decode(pd)
 			if err != nil {
-				return fmt.Errorf("failed to decode aborted transaction in partition response: %w", err)
+				if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+					return decodingErr.WithAddedContext(fmt.Sprintf("abortedTransaction[%d]", k))
+				}
+				return err
 			}
 			pr.AbortedTransactions[k] = abortedTransaction
 		}
 	}
 
 	if pr.PreferedReadReplica, err = pd.GetInt32(); err != nil {
-		return fmt.Errorf("failed to decode in func: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("preferedReadReplica")
+		}
+		return err
 	}
 
 	// Record Batches are encoded as COMPACT_RECORDS
 	numBytes, err := pd.GetUnsignedVarint()
 	if err != nil {
-		return fmt.Errorf("failed to decode in func: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("compactRecordsLength")
+		}
+		return err
 	}
 	numBytes -= 1
 
+	k := 0
 	for numBytes > 0 && pd.Remaining() > 10 {
 		recordBatch := RecordBatch{}
 		err := recordBatch.Decode(pd)
 		if err != nil {
-			return fmt.Errorf("failed to decode in func: %w", err)
+			if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+				return decodingErr.WithAddedContext(fmt.Sprintf("recordBatch[%d]", k))
+			}
+			return err
 		}
 		pr.Records = append(pr.Records, recordBatch)
+		k++
 	}
-	pd.GetEmptyTaggedFieldArray()
+	_, err = pd.GetEmptyTaggedFieldArray()
+	if err != nil {
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("taggedFieldArray")
+		}
+		return err
+	}
 
 	return nil
 }
@@ -169,11 +248,17 @@ type AbortedTransaction struct {
 
 func (ab *AbortedTransaction) Decode(pd *decoder.RealDecoder) (err error) {
 	if ab.ProducerID, err = pd.GetInt64(); err != nil {
-		return fmt.Errorf("failed to decode producerID in aborted transaction: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("producerID").WithAddedContext("abortedTransaction")
+		}
+		return err
 	}
 
 	if ab.FirstOffset, err = pd.GetInt64(); err != nil {
-		return fmt.Errorf("failed to decode firstOffset in aborted transaction: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("firstOffset").WithAddedContext("abortedTransaction")
+		}
+		return err
 	}
 
 	return nil
@@ -197,64 +282,106 @@ type RecordBatch struct {
 
 func (rb *RecordBatch) Decode(pd *decoder.RealDecoder) (err error) {
 	if rb.BaseOffset, err = pd.GetInt64(); err != nil {
-		return fmt.Errorf("failed to decode baseOffset in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("baseOffset")
+		}
+		return err
 	}
 
 	if rb.BatchLength, err = pd.GetInt32(); err != nil {
-		return fmt.Errorf("failed to decode batchLength in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("batchLength")
+		}
+		return err
 	}
 
 	if rb.PartitionLeaderEpoch, err = pd.GetInt32(); err != nil {
-		return fmt.Errorf("failed to decode partitionLeaderEpoch in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("partitionLeaderEpoch")
+		}
+		return err
 	}
 
 	if rb.Magic, err = pd.GetInt8(); err != nil {
-		return fmt.Errorf("failed to decode magicByte in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("magicByte")
+		}
+		return err
 	}
 
 	if rb.CRC, err = pd.GetInt32(); err != nil {
-		return fmt.Errorf("failed to decode CRC in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("CRC")
+		}
+		return err
 	}
 	// TODO: validate CRC
 
 	if rb.Attributes, err = pd.GetInt16(); err != nil {
-		return fmt.Errorf("failed to decode attributes in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("recordAttributes")
+		}
+		return err
 	}
 
 	if rb.LastOffsetDelta, err = pd.GetInt32(); err != nil {
-		return fmt.Errorf("failed to decode lastOffsetDelta in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("lastOffsetDelta")
+		}
+		return err
 	}
 
 	if rb.FirstTimestamp, err = pd.GetInt64(); err != nil {
-		return fmt.Errorf("failed to decode firstTimestamp in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("firstTimestamp")
+		}
+		return err
 	}
 
 	if rb.MaxTimestamp, err = pd.GetInt64(); err != nil {
-		return fmt.Errorf("failed to decode maxTimestamp in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("maxTimestamp")
+		}
+		return err
 	}
 
 	if rb.ProducerId, err = pd.GetInt64(); err != nil {
-		return fmt.Errorf("failed to decode producerId in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("producerId")
+		}
+		return err
 	}
 
 	if rb.ProducerEpoch, err = pd.GetInt16(); err != nil {
-		return fmt.Errorf("failed to decode producerEpoch in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("producerEpoch")
+		}
+		return err
 	}
 
 	if rb.BaseSequence, err = pd.GetInt32(); err != nil {
-		return fmt.Errorf("failed to decode baseSequence in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("baseSequence")
+		}
+		return err
 	}
 
 	numRecords, err := pd.GetInt32()
 	if err != nil {
-		return fmt.Errorf("failed to decode numRecords in record batch: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("numRecords")
+		}
+		return err
 	}
 
 	for i := 0; i < int(numRecords); i++ {
 		record := Record{}
 		err := record.Decode(pd)
 		if err != nil {
-			return fmt.Errorf("failed to decode record in record batch: %w", err)
+			if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+				return decodingErr.WithAddedContext(fmt.Sprintf("record[%d]", i))
+			}
+			return err
 		}
 		rb.Records = append(rb.Records, record)
 	}
@@ -275,34 +402,52 @@ type Record struct {
 func (r *Record) Decode(pd *decoder.RealDecoder) (err error) {
 	length, err := pd.GetUnsignedVarint()
 	if err != nil {
-		return fmt.Errorf("failed to decode length in record: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("length")
+		}
+		return err
 	}
 	r.Length = int32(length)
 
 	if r.Attributes, err = pd.GetInt8(); err != nil {
-		return fmt.Errorf("failed to decode attributes in record: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("attributes")
+		}
+		return err
 	}
 
 	if r.TimestampDelta, err = pd.GetSignedVarint(); err != nil {
-		return fmt.Errorf("failed to decode timestampDelta in record: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("timestampDelta")
+		}
+		return err
 	}
 
 	offsetDelta, err := pd.GetSignedVarint()
 	if err != nil {
-		return fmt.Errorf("failed to decode offsetDelta in record: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("offsetDelta")
+		}
+		return err
 	}
 	r.OffsetDelta = int32(offsetDelta)
 
 	keyLength, err := pd.GetSignedVarint()
 	if err != nil {
-		return fmt.Errorf("failed to decode keyLength in record: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("keyLength")
+		}
+		return err
 	}
 
 	var key []byte
 	if keyLength > 0 {
 		key, err = pd.GetRawBytes(int(keyLength) - 1)
 		if err != nil {
-			return fmt.Errorf("failed to decode key in record: %w", err)
+			if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+				return decodingErr.WithAddedContext("messageKey")
+			}
+			return err
 		}
 		r.Key = key
 	} else {
@@ -311,16 +456,28 @@ func (r *Record) Decode(pd *decoder.RealDecoder) (err error) {
 
 	valueLength, err := pd.GetSignedVarint()
 	if err != nil {
-		return fmt.Errorf("failed to decode valueLength in record: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("valueLength")
+		}
+		return err
 	}
 
 	value, err := pd.GetRawBytes(int(valueLength))
 	if err != nil {
-		return fmt.Errorf("failed to decode value in record: %w", err)
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("messageValue")
+		}
+		return err
 	}
 	r.Value = value
 
-	pd.GetEmptyTaggedFieldArray()
+	_, err = pd.GetEmptyTaggedFieldArray()
+	if err != nil {
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("taggedFieldArray")
+		}
+		return err
+	}
 
 	return nil
 }
