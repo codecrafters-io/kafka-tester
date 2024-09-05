e053a5c9afd00495f206bd3afe9dafcbd0d1d623
Ryan Gang <ryan-gg@outlook.com>
2024-09-05 14:48:40 +0530

    ci: install kafka on ci & update your_program

--- internal/test_helpers/pass_all/your_program.sh
+++ internal/test_helpers/pass_all/your_program.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 echo "Starting Kafka using KRaft (Kafka Raft Metadata)"
-cd /tmp
-rm -rf /tmp/kafka-logs /tmp/zookeeper /tmp/kraft-combined-logs
-cd /Users/ryang/Developer/tmp/kafka/kafka_2.13-3.8.0
 # For fresh start
 # KAFKA_CLUSTER_ID="$(./bin/kafka-storage.sh random-uuid)"
-# ./bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/kraft/server.properties
-./bin/kafka-server-start.sh config/kraft/server.properties --override log.dirs=/Users/ryang/Developer/work/course-testers/kafka-tester/internal/test_helpers/pass_all/kraft-combined-logs > /dev/null 2>&1
+# /kafka/bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/kraft/server.properties
+/usr/local/kafka/bin/kafka-server-start.sh config/kraft/server.properties --override log.dirs=./internal/test_helpers/pass_all/kraft-combined-logs > /dev/null 2>&1
