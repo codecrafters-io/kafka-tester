0207fa83de290cb8948503fdf164864ebb9c354d
Ryan Gang <ryan-gg@outlook.com>
2024-09-05 13:31:03 +0530

    test: add fixtures

--- internal/stages_test.go
+++ internal/stages_test.go
@@ -10,7 +10,15 @@ import (
 func TestStages(t *testing.T) {
 	os.Setenv("CODECRAFTERS_RANDOM_SEED", "1234567890")
 
-	testCases := map[string]tester_utils_testing.TesterOutputTestCase{}
+	testCases := map[string]tester_utils_testing.TesterOutputTestCase{
+		"transactions_pass": {
+			UntilStageSlug:      "st10",
+			CodePath:            "./test_helpers/pass_all",
+			ExpectedExitCode:    0,
+			StdoutFixturePath:   "./test_helpers/fixtures/base/pass",
+			NormalizeOutputFunc: normalizeTesterOutput,
+		},
+	}
 
 	tester_utils_testing.TestTesterOutput(t, testerDefinition, testCases)
 }
