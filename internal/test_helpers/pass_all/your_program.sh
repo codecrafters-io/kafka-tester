904da617b8358543f2b5dc2cbc4d9734713fd76d
Ryan <ryan-gg@outlook.com>
2024-09-05 09:36:18 +0000

    ci: update your_program.sh for testing against kafka

--- internal/test_helpers/pass_all/your_program.sh
+++ internal/test_helpers/pass_all/your_program.sh
@@ -3,4 +3,4 @@ echo "Starting Kafka using KRaft (Kafka Raft Metadata)"
 # For fresh start
 # KAFKA_CLUSTER_ID="$(./bin/kafka-storage.sh random-uuid)"
 # /kafka/bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/kraft/server.properties
-/usr/local/kafka/bin/kafka-server-start.sh config/kraft/server.properties --override log.dirs=./internal/test_helpers/pass_all/kraft-combined-logs > /dev/null 2>&1
+/usr/local/kafka/bin/kafka-server-start.sh ./internal/test_helpers/pass_all/kraft.server.properties --override log.dirs=./internal/test_helpers/pass_all/kraft-combined-logs > /dev/null 2>&1
