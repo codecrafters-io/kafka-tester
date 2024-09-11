16b225c395f2e9f4be73a1410fe08f3c76d7e9e9
Ryan Gang <ryan-gg@outlook.com>
2024-09-12 00:42:23 +0530

    feat: add placeholder for stages 4-5 of FETCH extension

---
+++ internal/stagef4.go
@@ -0,0 +1,11 @@
+package internal
+
+import (
+	"fmt"
+
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+func testSingleFetchFromDisk(stageHarness *test_case_harness.TestCaseHarness) error {
+	return fmt.Errorf("This stage is not implemented yet, we are working on it!")
+}
