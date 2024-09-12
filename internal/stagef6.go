44c8a4e2a13ce4196c6e712565435fd009720615
Ryan Gang <ryan-gg@outlook.com>
2024-09-12 20:07:33 +0530

    refactor: rename files to accomodate new stages

--- internal/stagef6.go
+++ internal/stagef6.go
@@ -6,6 +6,6 @@ import (
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
-func testSingleFetchFromDisk(stageHarness *test_case_harness.TestCaseHarness) error {
+func testMultiFetchFromDisk(stageHarness *test_case_harness.TestCaseHarness) error {
 	return fmt.Errorf("This stage is not implemented yet, we are working on it!")
 }
