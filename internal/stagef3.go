193739be0913c374c64724e2300656b2e92a94fc
Ryan Gang <ryan-gg@outlook.com>
2024-09-12 20:09:26 +0530

    feat: add new stage with unknown topic id

--- internal/stagef3.go
+++ internal/stagef3.go
@@ -3,7 +3,6 @@ package internal
 import (
 	"fmt"
 	"math"
-	"reflect"
 
 	"github.com/codecrafters-io/kafka-tester/internal/kafka_executable"
 	"github.com/codecrafters-io/kafka-tester/protocol"
@@ -12,7 +11,7 @@ import (
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
-func testFetch(stageHarness *test_case_harness.TestCaseHarness) error {
+func testFetchWithEmptyTopic(stageHarness *test_case_harness.TestCaseHarness) error {
 	b := kafka_executable.NewKafkaExecutable(stageHarness)
 	if err := b.Run(); err != nil {
 		return err
@@ -44,7 +43,7 @@ func testFetch(stageHarness *test_case_harness.TestCaseHarness) error {
 			FetchSessionEpoch: 0,
 			Topics: []kafkaapi.Topic{
 				{
-					TopicUUID: "bfd99e5e-3235-4552-81f8-d4af1741970c",
+					TopicUUID: "00000000-0000-0000-0000-000000000001",
 					Partitions: []kafkaapi.Partition{
 						{
 							ID:                 0,
@@ -87,32 +86,26 @@ func testFetch(stageHarness *test_case_harness.TestCaseHarness) error {
 	}
 	logger.Successf("✓ Error code: 0 (NO_ERROR)")
 
-	msgValues := []string{}
-	expectedMsgValues := []string{"Hello World!"}
-	for _, topicResponse := range responseBody.TopicResponses {
-		for _, partitionResponse := range topicResponse.PartitionResponses {
-			if len(partitionResponse.RecordBatches) == 0 {
-				return fmt.Errorf("Expected partition.RecordBatches to have length greater than 0, got %v", len(partitionResponse.RecordBatches))
-			}
-			for _, recordBatch := range partitionResponse.RecordBatches {
-				if len(recordBatch.Records) == 0 {
-					return fmt.Errorf("Expected recordBatch.Records to have length greater than 0, got %v", len(recordBatch.Records))
-				}
-				for _, r := range recordBatch.Records {
-					if r.Value == nil {
-						return fmt.Errorf("Expected record.Value to not be nil")
-					}
-					msgValues = append(msgValues, string(r.Value))
-				}
-			}
-		}
+	if len(responseBody.TopicResponses) != 1 {
+		return fmt.Errorf("Expected TopicResponses to have length 1, got %v", len(responseBody.TopicResponses))
 	}
 
-	if !reflect.DeepEqual(msgValues, expectedMsgValues) {
-		return fmt.Errorf("Expected message values to be %v, got %v", expectedMsgValues, msgValues)
+	topicResponse := responseBody.TopicResponses[0]
+	if len(topicResponse.PartitionResponses) != 1 {
+		return fmt.Errorf("Expected PartitionResponses to have length 1, got %v", len(topicResponse.PartitionResponses))
 	}
 
-	logger.Successf("✓ Messages: %q", msgValues)
+	partitionResponse := topicResponse.PartitionResponses[0]
+
+	if partitionResponse.ErrorCode != 100 {
+		return fmt.Errorf("Expected Error code to be 100, got %v", partitionResponse.ErrorCode)
+	}
+	logger.Successf("✓ PartitionResponse Error code: 100 (UNKNOWN_TOPIC_ID)")
+
+	if len(partitionResponse.RecordBatches) != 0 {
+		return fmt.Errorf("Expected RecordBatches to have length 0, got %v", len(partitionResponse.RecordBatches))
+	}
+	logger.Successf("✓ RecordBatches: %v", partitionResponse.RecordBatches)
 
 	return nil
 }
