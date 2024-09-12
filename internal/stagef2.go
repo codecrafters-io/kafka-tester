4c195e2f32271f981520ab629b4f35878c3be959
Ryan Gang <ryan-gg@outlook.com>
2024-09-12 20:07:14 +0530

    refactor: rename function name

--- internal/stagef2.go
+++ internal/stagef2.go
@@ -11,7 +11,7 @@ import (
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
-func testEmptyFetch(stageHarness *test_case_harness.TestCaseHarness) error {
+func testFetchWithNoTopics(stageHarness *test_case_harness.TestCaseHarness) error {
 	b := kafka_executable.NewKafkaExecutable(stageHarness)
 	if err := b.Run(); err != nil {
 		return err
