ea5390a8a04d51164c77291e99b2efd1fdca6f96
Ryan Gang <ryan-gg@outlook.com>
2024-09-18 13:03:54 +0530

    feat: update your_program.sh to use latest kafka release

--- internal/test_helpers/pass_all/your_program.sh
+++ internal/test_helpers/pass_all/your_program.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # echo "Starting Kafka using KRaft (Kafka Raft Metadata)"
-
+# For testing locally, you need to have the latest kafka version in /usr/local/bin/kafka-latest
+# Get it from here: https://github.com/codecrafters-io/build-your-own-kafka/blob/main/kafka_2.13-4.0.0-SNAPSHOT.tgz
+# Extract it and make sure you update the permissions
+# sudo chown -R user:group ./kafka-latest
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-/usr/local/kafka/bin/kafka-server-start.sh $SCRIPT_DIR/kraft.server.properties --override log.dirs=$SCRIPT_DIR/kraft-combined-logs > /dev/null 2>&1
+/usr/local/kafka-latest/bin/kafka-server-start.sh $SCRIPT_DIR/kraft.server.properties --override log.dirs=$SCRIPT_DIR/kraft-combined-logs  > /dev/null 2>&1
