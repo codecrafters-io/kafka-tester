274321d6ad3e9f9510790b96255b7ce22c86f966
Ryan Gang <ryan-gg@outlook.com>
2024-09-25 20:01:13 +0530

    refactor: update fetch request parameters

--- internal/stagef3.go
+++ internal/stagef3.go
@@ -6,6 +6,7 @@ import (
 	"github.com/codecrafters-io/kafka-tester/internal/kafka_executable"
 	"github.com/codecrafters-io/kafka-tester/protocol"
 	kafkaapi "github.com/codecrafters-io/kafka-tester/protocol/api"
+	"github.com/codecrafters-io/kafka-tester/protocol/serializer"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
@@ -14,11 +15,14 @@ func testFetchWithUnkownTopicID(stageHarness *test_case_harness.TestCaseHarness)
 	if err := b.Run(); err != nil {
 		return err
 	}
+	serializer.GenerateLogDirs()
 
 	logger := stageHarness.Logger
 
 	correlationId := getRandomCorrelationId()
-	UUID := "00000000-0000-0000-0000-000000000001"
+	UUID := "00000000-0000-0000-0000-000000000001" // ToDo: 1-9 for last digit
+	// ToDo: Remove :checkmarks: from decoder logs
+	// ToDo: Research on what is NULL v Empty arrays
 
 	broker := protocol.NewBroker("localhost:9092")
 	if err := broker.ConnectWithRetries(b, logger); err != nil {
