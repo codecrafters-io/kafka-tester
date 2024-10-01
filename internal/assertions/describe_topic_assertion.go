e85d6285157a08aed5d4f0e374ec92090dbe119d
Ryan Gang <ryan-gg@outlook.com>
2024-10-01 19:45:29 +0530

    feat: add describe_topic_assertion

--- internal/assertions/describe_topic_assertion.go
+++ internal/assertions/describe_topic_assertion.go
@@ -84,64 +84,50 @@ func (a *DescribeTopicPartitionsResponseAssertion) AssertTopics(fields []string)
 
 	return a
 }
+
+func (a *DescribeTopicPartitionsResponseAssertion) AssertPartitions(fields []string) *DescribeTopicPartitionsResponseAssertion {
+	if a.err != nil {
+		return a
+	}
+
+	for i, actualTopic := range a.ActualValue.Topics {
+		expectedTopic := a.ExpectedValue.Topics[i]
+		actualPartitions, expectedPartitions := actualTopic.Partitions, expectedTopic.Partitions
+
+		if len(actualPartitions) != len(expectedPartitions) {
+			a.err = fmt.Errorf("Expected %s to be %d, got %d", "partitions.length", len(expectedPartitions), len(actualPartitions))
+			return a
+		}
+
+		for j, actualPartition := range actualPartitions {
+			expectedPartition := expectedPartitions[j]
+
+			if Contains(fields, "ErrorCode") {
+				if actualPartition.ErrorCode != expectedPartition.ErrorCode {
+					// ToDo: Improve error message
+					a.err = fmt.Errorf("Expected %s to be %d, got %d", "Error Code", expectedPartition.ErrorCode, actualPartition.ErrorCode)
+					return a
+				}
+				a.logger.Successf("✓ PartitionResponse[%d] Error code: %d", j, actualPartition.ErrorCode)
+			}
+
+			if Contains(fields, "PartitionIndex") {
+				if actualPartition.PartitionIndex != expectedPartition.PartitionIndex {
+					a.err = fmt.Errorf("Expected %s to be %d, got %d", "Partition Index", expectedPartition.PartitionIndex, actualPartition.PartitionIndex)
+					return a
+				}
+				a.logger.Successf("✓ PartitionResponse[%d] Partition Index: %d", j, actualPartition.PartitionIndex)
+			}
+
+		}
+	}
+
+	return a
 }
 
-func (a DescribeTopicPartitionsResponseAssertion) Evaluate(firstLevelFields, secondLevelFields, thirdLevelFields []string, logger *logger.Logger) error {
+func (a DescribeTopicPartitionsResponseAssertion) Run() error {
 	// firstLevelFields: ["ThrottleTimeMs"]
 	// secondLevelFields (Topics): ["Name", "TopicID", "Partitions"]
 	// thirdLevelFields (Partitions): ["ID", "Leader", "Replicas", "Isr"]
-
-	if Contains(firstLevelFields, "ThrottleTimeMs") {
-		if a.ActualValue.ThrottleTimeMs != a.ExpectedValue.ThrottleTimeMs {
-			return fmt.Errorf("Expected %s to be %d, got %d", "ThrottleTimeMs", a.ExpectedValue.ThrottleTimeMs, a.ActualValue.ThrottleTimeMs)
-		}
-		logger.Successf("✓ Throttle Time: %d", a.ActualValue.ThrottleTimeMs)
-	}
-
-	if secondLevelFields != nil {
-		// If secondLevelFields is not nil, then we match the length of the topics array
-		if len(a.ActualValue.Topics) != len(secondLevelFields) {
-			return fmt.Errorf("Expected %s to have length %d, got %d", "TopicResponse", len(a.ExpectedValue.Topics), len(a.ActualValue.Topics))
-		}
-
-		// We will also assert each topic in the actual and expected arrays respectively
-		for i, actualTopic := range a.ActualValue.Topics {
-			expectedTopic := a.ExpectedValue.Topics[i]
-
-			if Contains(secondLevelFields, "ErrorCode") {
-				if actualTopic.ErrorCode != expectedTopic.ErrorCode {
-					return fmt.Errorf("Expected %s to be %d, got %d", fmt.Sprintf("TopicResponse[%d] Error Code", i), expectedTopic.ErrorCode, actualTopic.ErrorCode)
-				}
-
-				errorCodeName, ok := errorCodes[int(actualTopic.ErrorCode)]
-				if !ok {
-					errorCodeName = "UNKNOWN"
-				}
-				logger.Successf("✓ TopicResponse[%d] Error code: %d (%s)", i, actualTopic.ErrorCode, errorCodeName)
-			}
-
-			if Contains(secondLevelFields, "Name") {
-				if actualTopic.Name != expectedTopic.Name {
-					return fmt.Errorf("Expected %s to be %s, got %s", fmt.Sprintf("TopicResponse[%d] Topic Name", i), expectedTopic.Name, actualTopic.Name)
-				}
-				logger.Successf("✓ TopicResponse[%d] Topic Name: %s", i, actualTopic.Name)
-			}
-
-			if Contains(secondLevelFields, "TopicID") {
-				if actualTopic.TopicID != expectedTopic.TopicID {
-					return fmt.Errorf("Expected %s to be %s, got %s", fmt.Sprintf("TopicResponse[%d] Topic UUID", i), expectedTopic.TopicID, actualTopic.TopicID)
-				}
-				logger.Successf("✓ TopicResponse[%d] Topic UUID: %s", i, actualTopic.TopicID)
-			}
-
-			if thirdLevelFields != nil {
-				// If thirdLevelFields is not nil, then we match the length of the partitions array
-				if len(actualTopic.Partitions) != len(expectedTopic.Partitions) {
-					return fmt.Errorf("Expected %s to have length %d, got %d", "PartitionResponse", len(expectedTopic.Partitions), len(actualTopic.Partitions))
-				}
-			}
-		}
-	}
-
-	return nil
+	return a.err
 }
