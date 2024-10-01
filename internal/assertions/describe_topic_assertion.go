2f5a7008faf26c6d4e32dca67e09b5a4f6e4c4b5
Ryan Gang <ryan-gg@outlook.com>
2024-10-01 16:49:42 +0530

    feat: add builder pattern for asserting topics and main body

--- internal/assertions/describe_topic_assertion.go
+++ internal/assertions/describe_topic_assertion.go
@@ -10,10 +10,80 @@ import (
 type DescribeTopicPartitionsResponseAssertion struct {
 	ActualValue   kafkaapi.DescribeTopicPartitionsResponse
 	ExpectedValue kafkaapi.DescribeTopicPartitionsResponse
+	logger        *logger.Logger
+	err           error
 }
 
-func NewDescribeTopicPartitionsResponseAssertion(actualValue kafkaapi.DescribeTopicPartitionsResponse, expectedValue kafkaapi.DescribeTopicPartitionsResponse) DescribeTopicPartitionsResponseAssertion {
-	return DescribeTopicPartitionsResponseAssertion{ActualValue: actualValue, ExpectedValue: expectedValue}
+func NewDescribeTopicPartitionsResponseAssertion(actualValue kafkaapi.DescribeTopicPartitionsResponse, expectedValue kafkaapi.DescribeTopicPartitionsResponse, logger *logger.Logger) *DescribeTopicPartitionsResponseAssertion {
+	return &DescribeTopicPartitionsResponseAssertion{
+		ActualValue:   actualValue,
+		ExpectedValue: expectedValue,
+		logger:        logger,
+	}
+}
+
+func (a *DescribeTopicPartitionsResponseAssertion) AssertBody(fields []string) *DescribeTopicPartitionsResponseAssertion {
+	if a.err != nil {
+		return a
+	}
+	if Contains(fields, "ThrottleTimeMs") {
+		if a.ActualValue.ThrottleTimeMs != a.ExpectedValue.ThrottleTimeMs {
+			a.err = fmt.Errorf("Expected %s to be %d, got %d", "ThrottleTimeMs", a.ExpectedValue.ThrottleTimeMs, a.ActualValue.ThrottleTimeMs)
+			return a
+		}
+		a.logger.Successf("✓ Throttle Time: %d", a.ActualValue.ThrottleTimeMs)
+	}
+
+	return a
+}
+
+func (a *DescribeTopicPartitionsResponseAssertion) AssertTopics(fields []string) *DescribeTopicPartitionsResponseAssertion {
+	if a.err != nil {
+		return a
+	}
+
+	if len(a.ActualValue.Topics) != len(a.ExpectedValue.Topics) {
+		a.err = fmt.Errorf("Expected %s to be %d, got %d", "topics.length", len(a.ExpectedValue.Topics), len(a.ActualValue.Topics))
+		return a
+	}
+
+	for i, actualTopic := range a.ActualValue.Topics {
+		expectedTopic := a.ExpectedValue.Topics[i]
+		if Contains(fields, "ErrorCode") {
+			if actualTopic.ErrorCode != expectedTopic.ErrorCode {
+				a.err = fmt.Errorf("Expected %s to be %d, got %d", "Error Code", expectedTopic.ErrorCode, actualTopic.ErrorCode)
+				return a
+			}
+			a.logger.Successf("✓ TopicResponse[%d] Error code: %d", i, actualTopic.ErrorCode)
+		}
+
+		if Contains(fields, "Name") {
+			if actualTopic.Name != expectedTopic.Name {
+				a.err = fmt.Errorf("Expected %s to be %s, got %s", "Topic Name", expectedTopic.Name, actualTopic.Name)
+				return a
+			}
+			a.logger.Successf("✓ TopicResponse[%d] Topic Name: %s", i, actualTopic.Name)
+		}
+
+		if Contains(fields, "TopicID") {
+			if actualTopic.TopicID != expectedTopic.TopicID {
+				a.err = fmt.Errorf("Expected %s to be %s, got %s", "Topic UUID", expectedTopic.TopicID, actualTopic.TopicID)
+				return a
+			}
+			a.logger.Successf("✓ TopicResponse[%d] Topic UUID: %s", i, actualTopic.TopicID)
+		}
+
+		if Contains(fields, "TopicAuthorizedOperations") {
+			if actualTopic.TopicAuthorizedOperations != expectedTopic.TopicAuthorizedOperations {
+				a.err = fmt.Errorf("Expected %s to be %d, got %d", "Topic Authorized Operations", expectedTopic.TopicAuthorizedOperations, actualTopic.TopicAuthorizedOperations)
+				return a
+			}
+			a.logger.Successf("✓ TopicResponse[%d] Topic Authorized Operations: %d", i, actualTopic.TopicAuthorizedOperations)
+		}
+	}
+
+	return a
+}
 }
 
 func (a DescribeTopicPartitionsResponseAssertion) Evaluate(firstLevelFields, secondLevelFields, thirdLevelFields []string, logger *logger.Logger) error {
