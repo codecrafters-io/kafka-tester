689831d45232a76ca2f6041ed16bc90278d1c1dd
Ryan Gang <ryan-gg@outlook.com>
2025-01-06 11:52:38 +0530

    refactor: enhance logging and error handling in testSequentialRequests function
    - Replaced instances of logger with stageLogger for consistent logging throughout the function.
    - Improved error handling by ensuring proper closure of the broker using a deferred function.
    - Streamlined log messages for better clarity and maintainability.

--- internal/stagec1.go
+++ internal/stagec1.go
@@ -2,34 +2,35 @@ package internal
 
 import (
 	"fmt"
+	"github.com/codecrafters-io/tester-utils/logger"
 
 	"github.com/codecrafters-io/kafka-tester/internal/kafka_executable"
 	"github.com/codecrafters-io/kafka-tester/protocol"
 	kafkaapi "github.com/codecrafters-io/kafka-tester/protocol/api"
 	"github.com/codecrafters-io/kafka-tester/protocol/serializer"
-	"github.com/codecrafters-io/tester-utils/logger"
 	"github.com/codecrafters-io/tester-utils/random"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
 func testSequentialRequests(stageHarness *test_case_harness.TestCaseHarness) error {
 	b := kafka_executable.NewKafkaExecutable(stageHarness)
-	if err := b.Run(); err != nil {
-		return err
-	}
-
-	quietLogger := logger.GetQuietLogger("")
-	logger := stageHarness.Logger
-	err := serializer.GenerateLogDirs(quietLogger, true)
+	err := serializer.GenerateLogDirs(logger.GetQuietLogger(""), true)
 	if err != nil {
 		return err
 	}
 
-	broker := protocol.NewBroker("localhost:9092")
-	if err := broker.ConnectWithRetries(b, logger); err != nil {
+	stageLogger := stageHarness.Logger
+	if err := b.Run(); err != nil {
 		return err
 	}
-	defer broker.Close()
+
+	broker := protocol.NewBroker("localhost:9092")
+	if err := broker.ConnectWithRetries(b, stageLogger); err != nil {
+		return err
+	}
+	defer func(broker *protocol.Broker) {
+		_ = broker.Close()
+	}(broker)
 
 	requestCount := random.RandomInt(2, 5)
 	for i := 0; i < requestCount; i++ {
@@ -49,16 +50,16 @@ func testSequentialRequests(stageHarness *test_case_harness.TestCaseHarness) err
 		}
 
 		message := kafkaapi.EncodeApiVersionsRequest(&request)
-		logger.Infof("Sending request %v of %v: \"ApiVersions\" (version: %v) request (Correlation id: %v)", i+1, requestCount, request.Header.ApiVersion, request.Header.CorrelationId)
-		logger.Debugf("Hexdump of sent \"ApiVersions\" request: \n%v\n", GetFormattedHexdump(message))
+		stageLogger.Infof("Sending request %v of %v: \"ApiVersions\" (version: %v) request (Correlation id: %v)", i+1, requestCount, request.Header.ApiVersion, request.Header.CorrelationId)
+		stageLogger.Debugf("Hexdump of sent \"ApiVersions\" request: \n%v\n", GetFormattedHexdump(message))
 
 		response, err := broker.SendAndReceive(message)
 		if err != nil {
 			return err
 		}
-		logger.Debugf("Hexdump of received \"ApiVersions\" response: \n%v\n", GetFormattedHexdump(response.RawBytes))
+		stageLogger.Debugf("Hexdump of received \"ApiVersions\" response: \n%v\n", GetFormattedHexdump(response.RawBytes))
 
-		responseHeader, responseBody, err := kafkaapi.DecodeApiVersionsHeaderAndResponse(response.Payload, 3, logger)
+		responseHeader, responseBody, err := kafkaapi.DecodeApiVersionsHeaderAndResponse(response.Payload, 3, stageLogger)
 		if err != nil {
 			return err
 		}
@@ -66,17 +67,17 @@ func testSequentialRequests(stageHarness *test_case_harness.TestCaseHarness) err
 		if responseHeader.CorrelationId != correlationId {
 			return fmt.Errorf("Expected Correlation ID to be %v, got %v", correlationId, responseHeader.CorrelationId)
 		}
-		logger.Successf("✓ Correlation ID: %v", responseHeader.CorrelationId)
+		stageLogger.Successf("✓ Correlation ID: %v", responseHeader.CorrelationId)
 
 		if responseBody.ErrorCode != 0 {
 			return fmt.Errorf("Expected Error code to be 0, got %v", responseBody.ErrorCode)
 		}
-		logger.Successf("✓ Error code: 0 (NO_ERROR)")
+		stageLogger.Successf("✓ Error code: 0 (NO_ERROR)")
 
 		if len(responseBody.ApiKeys) < 1 {
 			return fmt.Errorf("Expected API keys array to include atleast 1 key (API_VERSIONS), got %v", len(responseBody.ApiKeys))
 		}
-		logger.Successf("✓ API keys array is non-empty")
+		stageLogger.Successf("✓ API keys array is non-empty")
 
 		foundAPIKey := false
 		MAX_VERSION_APIVERSION := int16(4)
@@ -84,7 +85,7 @@ func testSequentialRequests(stageHarness *test_case_harness.TestCaseHarness) err
 			if apiVersionKey.ApiKey == 18 {
 				foundAPIKey = true
 				if apiVersionKey.MaxVersion >= MAX_VERSION_APIVERSION {
-					logger.Successf("✓ API version %v is supported for API_VERSIONS", MAX_VERSION_APIVERSION)
+					stageLogger.Successf("✓ API version %v is supported for API_VERSIONS", MAX_VERSION_APIVERSION)
 				} else {
 					return fmt.Errorf("Expected API version %v to be supported for API_VERSIONS, got %v", MAX_VERSION_APIVERSION, apiVersionKey.MaxVersion)
 				}
@@ -95,7 +96,7 @@ func testSequentialRequests(stageHarness *test_case_harness.TestCaseHarness) err
 			return fmt.Errorf("Expected APIVersionsResponseKey to be present for API key 18 (API_VERSIONS)")
 		}
 
-		logger.Successf("✓ Test %v of %v: Passed", i+1, requestCount)
+		stageLogger.Successf("✓ Test %v of %v: Passed", i+1, requestCount)
 	}
 
 	return nil
