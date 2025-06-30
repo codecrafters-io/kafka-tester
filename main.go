3e16a9a7f934578fe1a2f409346107252c57ca96
Ryan <ryan-gg@outlook.com>
2025-06-30 21:36:05 +0530

    feat: add kafka produce extension adhoc tests

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"strings"
-	"time"
 
 	"github.com/codecrafters-io/kafka-tester/protocol"
 	kafkaapi "github.com/codecrafters-io/kafka-tester/protocol/api"
@@ -24,7 +23,10 @@ func main() {
 
 	// request := getUnknownTopicRequest(correlationId)
 	// request := getWrongPartitionRequest(correlationId)
-	request := getSuccessRequest(correlationId)
+	// request := getSuccessRequest(correlationId)
+	// request := getPR5Request(correlationId)
+	// request := getPR6MultiPartitionRequest(correlationId)
+	request := getPR7Request(correlationId)
 
 	message := kafkaapi.EncodeProduceRequest(&request)
 	stageLogger.Infof("Sending \"Produce\" (version: %v) request (Correlation id: %v)", request.Header.ApiVersion, request.Header.CorrelationId)
@@ -111,16 +113,16 @@ func getUnknownTopicRequest(correlationId int32) kafkaapi.ProduceRequest {
 	recordBatch := kafkaapi.RecordBatch{
 		BaseOffset:           0,
 		BatchLength:          0, // Will be calculated during encoding
-		PartitionLeaderEpoch: 0,
+		PartitionLeaderEpoch: -1,
 		Magic:                2, // Use magic byte 2 for current format
 		CRC:                  0, // Will be calculated during encoding
 		Attributes:           0,
 		LastOffsetDelta:      0,
-		FirstTimestamp:       time.Now().UnixMilli(),
-		MaxTimestamp:         time.Now().UnixMilli(),
-		ProducerId:           -1,
-		ProducerEpoch:        -1,
-		BaseSequence:         -1,
+		FirstTimestamp:       1718878078402,
+		MaxTimestamp:         1718878078402,
+		ProducerId:           0,
+		ProducerEpoch:        0,
+		BaseSequence:         0,
 		Records:              []kafkaapi.Record{record},
 	}
 
@@ -168,16 +170,16 @@ func getWrongPartitionRequest(correlationId int32) kafkaapi.ProduceRequest {
 	recordBatch := kafkaapi.RecordBatch{
 		BaseOffset:           0,
 		BatchLength:          0, // Will be calculated during encoding
-		PartitionLeaderEpoch: 0,
+		PartitionLeaderEpoch: -1,
 		Magic:                2, // Use magic byte 2 for current format
 		CRC:                  0, // Will be calculated during encoding
 		Attributes:           0,
 		LastOffsetDelta:      0,
-		FirstTimestamp:       time.Now().UnixMilli(),
-		MaxTimestamp:         time.Now().UnixMilli(),
-		ProducerId:           -1,
-		ProducerEpoch:        -1,
-		BaseSequence:         -1,
+		FirstTimestamp:       1718878078402,
+		MaxTimestamp:         1718878078402,
+		ProducerId:           0,
+		ProducerEpoch:        0,
+		BaseSequence:         0,
 		Records:              []kafkaapi.Record{record},
 	}
 
@@ -212,11 +214,9 @@ func getWrongPartitionRequest(correlationId int32) kafkaapi.ProduceRequest {
 func getSuccessRequest(correlationId int32) kafkaapi.ProduceRequest {
 	// Create a simple record to produce
 	record := kafkaapi.Record{
-		Length:         0, // Will be calculated during encoding
 		Attributes:     0,
 		TimestampDelta: 0,
-		OffsetDelta:    0,
-		Key:            nil, // No key
+		Key:            nil,
 		Value:          []byte("Hello Ryan"),
 		Headers:        []kafkaapi.RecordHeader{},
 	}
@@ -224,17 +224,14 @@ func getSuccessRequest(correlationId int32) kafkaapi.ProduceRequest {
 	// Create a record batch containing our record
 	recordBatch := kafkaapi.RecordBatch{
 		BaseOffset:           0,
-		BatchLength:          0, // Will be calculated during encoding
-		PartitionLeaderEpoch: 0,
-		Magic:                2, // Use magic byte 2 for current format
-		CRC:                  0, // Will be calculated during encoding
+		PartitionLeaderEpoch: -1,
 		Attributes:           0,
 		LastOffsetDelta:      0,
-		FirstTimestamp:       time.Now().UnixMilli(),
-		MaxTimestamp:         time.Now().UnixMilli(),
-		ProducerId:           -1,
-		ProducerEpoch:        -1,
-		BaseSequence:         -1,
+		FirstTimestamp:       1718878078402,
+		MaxTimestamp:         1718878078402,
+		ProducerId:           0,
+		ProducerEpoch:        0,
+		BaseSequence:         0,
 		Records:              []kafkaapi.Record{record},
 	}
 
@@ -256,6 +253,263 @@ func getSuccessRequest(correlationId int32) kafkaapi.ProduceRequest {
 						{
 							Index:   0, // Valid partition
 							Records: []kafkaapi.RecordBatch{recordBatch},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return request
+}
+
+// getPR5Request returns a Produce request with multiple records in a single batch (Stage PR5)
+func getPR5Request(correlationId int32) kafkaapi.ProduceRequest {
+	// Create multiple records for the batch
+	record1 := kafkaapi.Record{
+		Attributes:     0,
+		TimestampDelta: 0,
+		Key:            nil,
+		Value:          []byte("Ryan's Message 4"),
+		Headers:        []kafkaapi.RecordHeader{},
+	}
+
+	record2 := kafkaapi.Record{
+		Attributes:     0,
+		TimestampDelta: 0,
+		Key:            nil,
+		Value:          []byte("Ryan's Message 5"),
+		Headers:        []kafkaapi.RecordHeader{},
+	}
+
+	record3 := kafkaapi.Record{
+		Attributes:     0,
+		TimestampDelta: 0,
+		Key:            nil,
+		Value:          []byte("Ryan's Message 6"),
+		Headers:        []kafkaapi.RecordHeader{},
+	}
+
+	// Create a record batch containing multiple records
+	recordBatch := kafkaapi.RecordBatch{
+		BaseOffset:           0,
+		PartitionLeaderEpoch: -1,
+		Attributes:           0,
+		LastOffsetDelta:      2, // Number of records - 1 (3 records: 0, 1, 2)
+		FirstTimestamp:       1718878078402,
+		MaxTimestamp:         1718878078402,
+		ProducerId:           0,
+		ProducerEpoch:        0,
+		BaseSequence:         0,
+		Records:              []kafkaapi.Record{record1, record2, record3},
+	}
+
+	request := kafkaapi.ProduceRequest{
+		Header: kafkaapi.RequestHeader{
+			ApiKey:        0,
+			ApiVersion:    11,
+			CorrelationId: correlationId,
+			ClientId:      "kafka-tester",
+		},
+		Body: kafkaapi.ProduceRequestBody{
+			TransactionalID: "", // Empty string for non-transactional
+			Acks:            1,  // Wait for leader acknowledgment
+			TimeoutMs:       5000,
+			Topics: []kafkaapi.TopicData{
+				{
+					Name: "ryanproduce", // Existing topic with 3 partitions (0, 1, 2)
+					Partitions: []kafkaapi.PartitionData{
+						{
+							Index:   0, // Valid partition
+							Records: []kafkaapi.RecordBatch{recordBatch},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return request
+}
+
+// getPR6MultiPartitionRequest returns a Produce request with multiple partitions (Stage PR6)
+func getPR6MultiPartitionRequest(correlationId int32) kafkaapi.ProduceRequest {
+	// Create records for partition 0
+	record1P0 := kafkaapi.Record{
+		Attributes:     0,
+		TimestampDelta: 0,
+		Key:            nil,
+		Value:          []byte("Ryan's Message for partition 0"),
+		Headers:        []kafkaapi.RecordHeader{},
+	}
+
+	recordBatch1 := kafkaapi.RecordBatch{
+		BaseOffset:           0,
+		PartitionLeaderEpoch: -1,
+		Attributes:           0,
+		LastOffsetDelta:      0, // Single record
+		FirstTimestamp:       1718878078402,
+		MaxTimestamp:         1718878078402,
+		ProducerId:           0,
+		ProducerEpoch:        0,
+		BaseSequence:         0,
+		Records:              []kafkaapi.Record{record1P0},
+	}
+
+	// Create records for partition 1
+	record1P1 := kafkaapi.Record{
+		Attributes:     0,
+		TimestampDelta: 0,
+		Key:            nil,
+		Value:          []byte("Ryan's Message for partition 1"),
+		Headers:        []kafkaapi.RecordHeader{},
+	}
+
+	recordBatch2 := kafkaapi.RecordBatch{
+		BaseOffset:           0,
+		PartitionLeaderEpoch: -1,
+		Attributes:           0,
+		LastOffsetDelta:      0, // Single record
+		FirstTimestamp:       1718878078402,
+		MaxTimestamp:         1718878078402,
+		ProducerId:           0,
+		ProducerEpoch:        0,
+		BaseSequence:         0,
+		Records:              []kafkaapi.Record{record1P1},
+	}
+
+	// Create records for partition 2
+	record1P2 := kafkaapi.Record{
+		Attributes:     0,
+		TimestampDelta: 0,
+		Key:            nil,
+		Value:          []byte("Ryan's Message for partition 2"),
+		Headers:        []kafkaapi.RecordHeader{},
+	}
+
+	recordBatch3 := kafkaapi.RecordBatch{
+		BaseOffset:           0,
+		PartitionLeaderEpoch: -1,
+		Attributes:           0,
+		LastOffsetDelta:      0, // Single record
+		FirstTimestamp:       1718878078402,
+		MaxTimestamp:         1718878078402,
+		ProducerId:           0,
+		ProducerEpoch:        0,
+		BaseSequence:         0,
+		Records:              []kafkaapi.Record{record1P2},
+	}
+
+	request := kafkaapi.ProduceRequest{
+		Header: kafkaapi.RequestHeader{
+			ApiKey:        0,
+			ApiVersion:    11,
+			CorrelationId: correlationId,
+			ClientId:      "kafka-tester",
+		},
+		Body: kafkaapi.ProduceRequestBody{
+			TransactionalID: "", // Empty string for non-transactional
+			Acks:            1,  // Wait for leader acknowledgment
+			TimeoutMs:       5000,
+			Topics: []kafkaapi.TopicData{
+				{
+					Name: "ryanproduce", // Same topic but multiple partitions
+					Partitions: []kafkaapi.PartitionData{
+						{
+							Index:   0, // Partition 0
+							Records: []kafkaapi.RecordBatch{recordBatch1},
+						},
+						{
+							Index:   1, // Partition 1
+							Records: []kafkaapi.RecordBatch{recordBatch2},
+						},
+						{
+							Index:   2, // Partition 2
+							Records: []kafkaapi.RecordBatch{recordBatch3},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return request
+}
+
+// getPR7Request returns a Produce request with multiple topics (Stage PR6)
+// Multiple topics
+func getPR7Request(correlationId int32) kafkaapi.ProduceRequest {
+	// Create records for first topic
+	record1Topic1 := kafkaapi.Record{
+		Attributes:     0,
+		TimestampDelta: 0,
+		Key:            nil,
+		Value:          []byte("Message for ryanproduce"),
+		Headers:        []kafkaapi.RecordHeader{},
+	}
+
+	recordBatch1 := kafkaapi.RecordBatch{
+		BaseOffset:           0,
+		PartitionLeaderEpoch: -1,
+		Attributes:           0,
+		LastOffsetDelta:      0, // Single record
+		FirstTimestamp:       1718878078402,
+		MaxTimestamp:         1718878078402,
+		ProducerId:           0,
+		ProducerEpoch:        0,
+		BaseSequence:         0,
+		Records:              []kafkaapi.Record{record1Topic1},
+	}
+
+	// Create records for second topic
+	record1Topic2 := kafkaapi.Record{
+		Attributes:     0,
+		TimestampDelta: 0,
+		Key:            nil,
+		Value:          []byte("Message for foo"),
+		Headers:        []kafkaapi.RecordHeader{},
+	}
+
+	recordBatch2 := kafkaapi.RecordBatch{
+		BaseOffset:           0,
+		PartitionLeaderEpoch: -1,
+		Attributes:           0,
+		LastOffsetDelta:      0, // Single record
+		FirstTimestamp:       1718878078402,
+		MaxTimestamp:         1718878078402,
+		ProducerId:           0,
+		ProducerEpoch:        0,
+		BaseSequence:         0,
+		Records:              []kafkaapi.Record{record1Topic2},
+	}
+
+	request := kafkaapi.ProduceRequest{
+		Header: kafkaapi.RequestHeader{
+			ApiKey:        0,
+			ApiVersion:    11,
+			CorrelationId: correlationId,
+			ClientId:      "kafka-tester",
+		},
+		Body: kafkaapi.ProduceRequestBody{
+			TransactionalID: "", // Empty string for non-transactional
+			Acks:            1,  // Wait for leader acknowledgment
+			TimeoutMs:       5000,
+			Topics: []kafkaapi.TopicData{
+				{
+					Name: "ryanproduce", // First topic
+					Partitions: []kafkaapi.PartitionData{
+						{
+							Index:   0,
+							Records: []kafkaapi.RecordBatch{recordBatch1},
+						},
+					},
+				},
+				{
+					Name: "foo", // Second topic (used in existing tests)
+					Partitions: []kafkaapi.PartitionData{
+						{
+							Index:   0,
+							Records: []kafkaapi.RecordBatch{recordBatch2},
 						},
 					},
 				},
