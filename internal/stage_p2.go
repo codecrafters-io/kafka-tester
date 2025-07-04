d75de1782d93449962ff02fc1f48b75febc214bc
Ryan <ryan-gg@outlook.com>
2025-07-04 23:49:51 +0530

    refactor: swap testProduce1 and testProduce2 implementations
    - Moved the implementation of the testProduce1 function to stage_p2.go and renamed it to testProduce2.
    - Introduced the testProduce1 function in stage_p1.go, which includes the complete logic for producing messages to Kafka, enhancing the structure of the test cases.
    - Updated imports and constants accordingly to maintain functionality and clarity in the codebase.

--- internal/stage_p2.go
+++ internal/stage_p2.go
@@ -1,9 +1,71 @@
 package internal
 
 import (
+	"github.com/codecrafters-io/kafka-tester/internal/kafka_executable"
+	"github.com/codecrafters-io/kafka-tester/protocol"
+	kafkaapi "github.com/codecrafters-io/kafka-tester/protocol/api"
+	"github.com/codecrafters-io/kafka-tester/protocol/builder"
+	"github.com/codecrafters-io/kafka-tester/protocol/serializer"
+	"github.com/codecrafters-io/tester-utils/logger"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
+const UNKNOWN_TOPIC = "test-topic"
+
 func testProduce2(stageHarness *test_case_harness.TestCaseHarness) error {
+	b := kafka_executable.NewKafkaExecutable(stageHarness)
+	stageLogger := stageHarness.Logger
+	err := serializer.GenerateLogDirs(logger.GetQuietLogger(""), false)
+	if err != nil {
+		return err
+	}
+
+	if err := b.Run(); err != nil {
+		return err
+	}
+
+	correlationId := getRandomCorrelationId()
+	broker := protocol.NewBroker("localhost:9092")
+	if err := broker.ConnectWithRetries(b, stageLogger); err != nil {
+		return err
+	}
+	defer func(broker *protocol.Broker) {
+		_ = broker.Close()
+	}(broker)
+
+	request := kafkaapi.ProduceRequest{
+		Header: builder.NewHeaderBuilder().
+			BuildProduceRequestHeader(correlationId),
+		Body: builder.NewRequestBuilder("produce").
+			WithTopic(UNKNOWN_TOPIC).
+			WithRecordBatch("Hello from Ryan!").
+			BuildProduceRequest(),
+	}
+
+	message := kafkaapi.EncodeProduceRequest(&request)
+	stageLogger.Infof("Sending \"Produce\" (version: %v) request (Correlation id: %v)", request.Header.ApiVersion, request.Header.CorrelationId)
+	stageLogger.Debugf("Hexdump of sent \"Produce\" request: \n%v\n", GetFormattedHexdump(message))
+
+	response, err := broker.SendAndReceive(message)
+	if err != nil {
+		stageLogger.Errorf("Failed to send and receive \"Produce\" request: %v", err)
+		return err
+	}
+	stageLogger.Debugf("Hexdump of received \"Produce\" response: \n%v\n", GetFormattedHexdump(response.RawBytes))
+
+	responseHeader, responseBody, err := kafkaapi.DecodeProduceHeaderAndResponse(response.Payload, 11, stageLogger)
+	if err != nil {
+		stageLogger.Errorf("Failed to decode \"Produce\" response header: %v", err)
+		return err
+	}
+
+	if responseHeader.CorrelationId != int32(correlationId) {
+		stageLogger.Errorf("Expected Correlation ID to be %v, got %v", correlationId, responseHeader.CorrelationId)
+		return err
+	}
+	stageLogger.Successf("✓ Correlation ID: %v", responseHeader.CorrelationId)
+	stageLogger.Successf("✓ Produce request/response cycle completed!")
+
+	stageLogger.Successf("✓ Produce response body: %v", responseBody.Responses)
 	return nil
 }
