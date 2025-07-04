a35577952add77719c35925cdeb80eee608c4689
Ryan <ryan-gg@outlook.com>
2025-07-04 18:18:55 +0530

    feat: add Dockerfile and test script for local Kafka testing setup
    - Introduced a Dockerfile to set up a local environment for testing with Kafka, including necessary packages and installation steps.
    - Added a test script to build the Docker image and run tests, facilitating local development and testing processes.

---
+++ local_testing/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Ensure we're in the correct directory
+cd "$(dirname "$0")/.."
+
+# Build and run
+docker build -t local-kafka-tester -f local_testing/Dockerfile .
+# Run make test
+# docker run --rm -it -v $(pwd):/app local-git-tester make test
+# Generate fixtures
+# docker run --rm -it -e CODECRAFTERS_RECORD_FIXTURES=true -v $(pwd):/app local-git-tester make test
+# Run test_all
+docker run --rm -it -v $(pwd):/app local-kafka-tester make test_all
