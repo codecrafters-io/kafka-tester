d53846846767397deef0bc1ab22d0f1ca7f8aab2
Ryan <ryan-gg@outlook.com>
2025-07-05 13:25:25 +0530

    refactor: consolidate constant declarations in constants.go
    - Combined multiple individual constant declarations into grouped declarations for improved readability and maintainability.
    - Added initialization for TOPIC4_NAME and TOPIC4_UUID in the init function to support the new topic functionality.
    - Updated random topic UUID generation to include the fourth topic, enhancing the overall structure of topic management.

--- protocol/common/constants.go
+++ protocol/common/constants.go
@@ -21,25 +21,13 @@ const (
 
 var (
 	// Topic names and UUIDs - initialized in init()
-	TOPIC1_NAME       string
-	TOPIC2_NAME       string
-	TOPIC3_NAME       string
-	TOPIC4_NAME       string
-	TOPIC1_UUID       string
-	TOPIC2_UUID       string
-	TOPIC3_UUID       string
-	TOPIC4_UUID       string
-	TOPICX_UUID       string
-	TOPIC_UNKOWN_NAME string
-	TOPIC_UNKOWN_UUID string
+	TOPIC1_NAME, TOPIC2_NAME, TOPIC3_NAME, TOPIC4_NAME string
+	TOPIC1_UUID, TOPIC2_UUID, TOPIC3_UUID, TOPIC4_UUID string
+	TOPICX_UUID                                        string
+	TOPIC_UNKOWN_NAME, TOPIC_UNKOWN_UUID               string
 
-	MESSAGE1 string
-	MESSAGE2 string
-	MESSAGE3 string
-
-	HELLO_MSG1 string
-	HELLO_MSG2 string
-	HELLO_MSG3 string
+	MESSAGE1, MESSAGE2, MESSAGE3       string
+	HELLO_MSG1, HELLO_MSG2, HELLO_MSG3 string
 )
 
 func init() {
@@ -50,10 +38,12 @@ func init() {
 	TOPIC1_NAME = topic_names[0]
 	TOPIC2_NAME = topic_names[1]
 	TOPIC3_NAME = topic_names[2]
-	random_topic_uuids := random.RandomInts(10, 100, 3)
+	TOPIC4_NAME = topic_names[3]
+	random_topic_uuids := random.RandomInts(10, 100, 4)
 	TOPIC1_UUID = fmt.Sprintf("00000000-0000-4000-8000-0000000000%02d", random_topic_uuids[0])
 	TOPIC2_UUID = fmt.Sprintf("00000000-0000-4000-8000-0000000000%02d", random_topic_uuids[1])
 	TOPIC3_UUID = fmt.Sprintf("00000000-0000-4000-8000-0000000000%02d", random_topic_uuids[2])
+	TOPIC4_UUID = fmt.Sprintf("00000000-0000-4000-8000-0000000000%02d", random_topic_uuids[3])
 	TOPICX_UUID = fmt.Sprintf("00000000-0000-0000-0000-00000000%04d", random.RandomInt(1000, 9999)) // Unknown topic used in requests
 
 	TOPIC_UNKOWN_NAME = fmt.Sprintf("unknown-topic-%s", topic_names[3])
