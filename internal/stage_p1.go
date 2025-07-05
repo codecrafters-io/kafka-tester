e49a2295761082e0ce2d9ba855abb390054f6248
Ryan <ryan-gg@outlook.com>
2025-07-05 22:17:08 +0530

    feat: implement ApiVersions request handling in testProduce1
    - Added the testProduce1 function to handle the ApiVersions request, including the construction and sending of the request to the Kafka broker.
    - Implemented response validation for the ApiVersions response, ensuring proper assertion of the response header and body against expected values.
    - Enhanced logging for both request and response, improving traceability during testing.

--- internal/stage_p1.go
+++ internal/stage_p1.go
@@ -1,9 +1,89 @@
 package internal
 
 import (
+	"github.com/codecrafters-io/kafka-tester/internal/assertions"
+	"github.com/codecrafters-io/kafka-tester/internal/kafka_executable"
+	"github.com/codecrafters-io/kafka-tester/protocol"
+	kafkaapi "github.com/codecrafters-io/kafka-tester/protocol/api"
+	"github.com/codecrafters-io/kafka-tester/protocol/builder"
+	"github.com/codecrafters-io/kafka-tester/protocol/serializer"
+	"github.com/codecrafters-io/tester-utils/logger"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
 func testProduce1(stageHarness *test_case_harness.TestCaseHarness) error {
+	b := kafka_executable.NewKafkaExecutable(stageHarness)
+	err := serializer.GenerateLogDirs(logger.GetQuietLogger(""), false)
+	if err != nil {
+		return err
+	}
+
+	stageLogger := stageHarness.Logger
+	if err := b.Run(); err != nil {
+		return err
+	}
+
+	correlationId := getRandomCorrelationId()
+
+	broker := protocol.NewBroker("localhost:9092")
+	if err := broker.ConnectWithRetries(b, stageLogger); err != nil {
+		return err
+	}
+	defer func(broker *protocol.Broker) {
+		_ = broker.Close()
+	}(broker)
+
+	request := kafkaapi.ApiVersionsRequest{
+		Header: builder.NewHeaderBuilder().BuildApiVersionsRequestHeader(correlationId),
+		Body: kafkaapi.ApiVersionsRequestBody{
+			Version:               4,
+			ClientSoftwareName:    "kafka-cli",
+			ClientSoftwareVersion: "0.1",
+		},
+	}
+
+	message := kafkaapi.EncodeApiVersionsRequest(&request)
+	stageLogger.Infof("Sending \"ApiVersions\" (version: %v) request (Correlation id: %v)", request.Header.ApiVersion, request.Header.CorrelationId)
+	stageLogger.Debugf("Hexdump of sent \"ApiVersions\" request: \n%v\n", GetFormattedHexdump(message))
+
+	response, err := broker.SendAndReceive(message)
+	if err != nil {
+		return err
+	}
+	stageLogger.Debugf("Hexdump of received \"ApiVersions\" response: \n%v\n", GetFormattedHexdump(response.RawBytes))
+
+	responseHeader, responseBody, err := kafkaapi.DecodeApiVersionsHeaderAndResponse(response.Payload, 3, stageLogger)
+	if err != nil {
+		return err
+	}
+
+	expectedResponseHeader := kafkaapi.ResponseHeader{
+		CorrelationId: correlationId,
+	}
+	if err = assertions.NewResponseHeaderAssertion(*responseHeader, expectedResponseHeader, stageLogger).AssertHeader([]string{"CorrelationId"}).Run(); err != nil {
+		return err
+	}
+
+	expectedApiVersionResponse := kafkaapi.ApiVersionsResponse{
+		Version:   3,
+		ErrorCode: 0,
+		ApiKeys: []kafkaapi.ApiVersionsResponseKey{
+			{
+				ApiKey:     0,
+				MaxVersion: 11,
+				MinVersion: 0,
+			},
+			{
+				ApiKey:     18,
+				MaxVersion: 4,
+				MinVersion: 0,
+			},
+		},
+	}
+
+	if err = assertions.NewApiVersionsResponseAssertion(*responseBody, expectedApiVersionResponse).Evaluate([]string{"ErrorCode"}, true, stageLogger); err != nil {
+		return err
+	}
+
 	return nil
 }
