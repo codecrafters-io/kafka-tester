26ee25581264e332e19d66dbec72201620a70526
Ryan <ryan-gg@outlook.com>
2025-07-09 16:37:15 +0530

    refactor: update topic assertions in DescribeTopicPartitionsResponseAssertion
    - Replaced AssertTopics with AssertOnlyTopics in multiple test functions to streamline topic assertions by excluding partition-related fields.
    - Updated AssertTopicsAndPartitions method to enhance clarity in assertions involving both topics and partitions.
    - These changes improve the maintainability and readability of the assertion process across test cases.

--- internal/stage_dtp2.go
+++ internal/stage_dtp2.go
@@ -80,7 +80,6 @@ func testDTPartitionWithUnknownTopic(stageHarness *test_case_harness.TestCaseHar
 
 	return assertions.NewDescribeTopicPartitionsResponseAssertion(*responseBody, expectedDescribeTopicPartitionsResponse, stageLogger).
 		AssertBody().
-		AssertTopics().
-		AssertPartitions().
+		AssertOnlyTopics().
 		Run()
 }
