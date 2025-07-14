e5356d35dd8697c90cfff20415d033d8c6a8d55e
Ryan <ryan-gg@outlook.com>
2025-07-14 16:17:17 +0530

    feat: implement CreateTopicRequest API structure
    - Added new types and encoding methods for CreateTopicRequest, including TopicData, AssignmentData, and ConfigData.
    - Implemented the Encode methods for each type to facilitate serialization in the Kafka API.
    - Established the CreateTopicRequestBody structure to encapsulate topic creation parameters.

---
+++ protocol/api/create_topic_request.go
@@ -0,0 +1,77 @@
+package kafkaapi
+
+import (
+	"github.com/codecrafters-io/kafka-tester/protocol/encoder"
+)
+
+type ConfigData struct {
+	Name  string
+	Value string
+}
+
+func (c *ConfigData) Encode(pe *encoder.RealEncoder) {
+	pe.PutCompactString(c.Name)
+	pe.PutNullableCompactString(&c.Value)
+	pe.PutEmptyTaggedFieldArray()
+}
+
+type AssignmentData struct {
+	PartitionIndex int32
+	BrokerIds      []int32
+}
+
+func (a *AssignmentData) Encode(pe *encoder.RealEncoder) {
+	pe.PutInt32(a.PartitionIndex)
+	pe.PutCompactArrayLength(len(a.BrokerIds))
+	pe.PutNullableCompactInt32Array(a.BrokerIds)
+	pe.PutEmptyTaggedFieldArray()
+}
+
+type TopicData struct {
+	Name              string // topic name
+	NumPartitions     int32  // number of partitions
+	ReplicationFactor int16  // replication factor
+	Assignments       []AssignmentData
+	Configs           []ConfigData
+}
+
+func (t *TopicData) Encode(pe *encoder.RealEncoder) {
+	pe.PutCompactString(t.Name)
+	pe.PutInt32(t.NumPartitions)
+	pe.PutInt16(t.ReplicationFactor)
+	pe.PutCompactArrayLength(len(t.Assignments))
+	for _, assignment := range t.Assignments {
+		assignment.Encode(pe)
+	}
+	pe.PutCompactArrayLength(len(t.Configs))
+	for _, config := range t.Configs {
+		config.Encode(pe)
+	}
+	pe.PutEmptyTaggedFieldArray()
+}
+
+type CreateTopicRequestBody struct {
+	Topics       []TopicData // topics to create
+	TimeoutMs    int32       // timeout in milliseconds
+	ValidateOnly bool        // validate only
+}
+
+func (pr *CreateTopicRequestBody) Encode(pe *encoder.RealEncoder) {
+	pe.PutCompactArrayLength(len(pr.Topics))
+	for _, topic := range pr.Topics {
+		topic.Encode(pe)
+	}
+	pe.PutInt32(pr.TimeoutMs)
+	pe.PutBool(pr.ValidateOnly)
+	pe.PutEmptyTaggedFieldArray()
+}
+
+type CreateTopicRequest struct {
+	Header RequestHeader
+	Body   CreateTopicRequestBody
+}
+
+func (r *CreateTopicRequest) Encode(pe *encoder.RealEncoder) {
+	r.Header.EncodeV2(pe)
+	r.Body.Encode(pe)
+}
