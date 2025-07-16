0a5695045c49abd4c3a0516ee77fb6c384ca2774
Ryan <ryan-gg@outlook.com>
2025-07-16 18:14:14 +0530

    refactor: remove unused DecodeProduceHeader function
    - Eliminated the DecodeProduceHeader function from create_topic.go as it was no longer needed.
    - This change streamlines the codebase and focuses on the relevant decoding methods for CreateTopic API.
    - The removal aligns with recent updates to improve the overall structure and clarity of the API's response handling.

--- protocol/api/create_topic.go
+++ protocol/api/create_topic.go
@@ -20,25 +20,6 @@ func EncodeCreateTopicRequest(request *CreateTopicRequest) []byte {
 	return message
 }
 
-func DecodeProduceHeader(response []byte, version int16, logger *logger.Logger) (*ResponseHeader, error) {
-	decoder := realdecoder.RealDecoder{}
-	decoder.Init(response)
-	logger.UpdateSecondaryPrefixes([]string{"Decoder"})
-	defer logger.ResetSecondaryPrefixes()
-
-	responseHeader := ResponseHeader{}
-	logger.Debugf("- .ResponseHeader")
-	if err := responseHeader.DecodeV1(&decoder, logger, 1); err != nil {
-		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
-			detailedError := decodingErr.WithAddedContext("Response Header").WithAddedContext("Produce Response v11")
-			return nil, decoder.FormatDetailedError(detailedError.Error())
-		}
-		return nil, err
-	}
-
-	return &responseHeader, nil
-}
-
 func DecodeCreateTopicHeaderAndResponse(response []byte, version int16, logger *logger.Logger) (*ResponseHeader, *CreateTopicResponseBody, error) {
 	decoder := realdecoder.RealDecoder{}
 	decoder.Init(response)
