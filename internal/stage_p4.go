d138ba39f540e3a50d68d6ba6c141e6e234c9a60
Ryan <ryan-gg@outlook.com>
2025-07-17 14:33:00 +0530

    feat: enhance testProduce4 with additional Produce request handling and logging
    - Introduced a second Produce request in the testProduce4 function to validate message production and response handling.
    - Added a sleep interval to ensure proper timing between requests, improving the reliability of the test.
    - Enhanced logging for both Produce requests, providing better insights into the request-response lifecycle and validation processes.

--- internal/stage_p4.go
+++ internal/stage_p4.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"time"
+
 	"github.com/codecrafters-io/kafka-tester/internal/assertions"
 	"github.com/codecrafters-io/kafka-tester/internal/kafka_executable"
 	"github.com/codecrafters-io/kafka-tester/protocol"
@@ -8,7 +10,6 @@ import (
 	"github.com/codecrafters-io/kafka-tester/protocol/builder"
 	"github.com/codecrafters-io/kafka-tester/protocol/common"
 	"github.com/codecrafters-io/kafka-tester/protocol/serializer"
-	"github.com/codecrafters-io/tester-utils/random"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
@@ -36,8 +37,8 @@ func testProduce4(stageHarness *test_case_harness.TestCaseHarness) error {
 	// 1
 
 	existingTopic := common.TOPIC4_NAME
-	existingPartition := int32(random.RandomInt(0, 3))
-	request := kafkaapi.ProduceRequest{
+	existingPartition := int32(1)
+	request1 := kafkaapi.ProduceRequest{
 		Header: builder.NewRequestHeaderBuilder().
 			BuildProduceRequestHeader(correlationId),
 		Body: builder.NewProduceRequestBuilder().
@@ -45,8 +46,8 @@ func testProduce4(stageHarness *test_case_harness.TestCaseHarness) error {
 			Build(),
 	}
 
-	message := kafkaapi.EncodeProduceRequest(&request)
-	stageLogger.Infof("Sending \"Produce\" (version: %v) request (Correlation id: %v)", request.Header.ApiVersion, request.Header.CorrelationId)
+	message := kafkaapi.EncodeProduceRequest(&request1)
+	stageLogger.Infof("Sending \"Produce\" (version: %v) request (Correlation id: %v)", request1.Header.ApiVersion, request1.Header.CorrelationId)
 	stageLogger.Debugf("Hexdump of sent \"Produce\" request: \n%v\n", GetFormattedHexdump(message))
 
 	response, err := broker.SendAndReceive(message)
@@ -81,7 +82,64 @@ func testProduce4(stageHarness *test_case_harness.TestCaseHarness) error {
 	}
 
 	// Validate RecordBatch in log file
-	expectedBatch := buildExpectedRecordBatchForStageP5()
+	expectedBatch := buildExpectedRecordBatchForStageP4()
+	err = validateRecordBatchInLogFile(existingTopic, existingPartition, expectedBatch, stageLogger)
+	if err != nil {
+		return err
+	}
+
+	stageLogger.Infof("Validated RecordBatch in log file for topic %s, partition %d", existingTopic, existingPartition)
+	stageLogger.Infof("Sleeping for 1 second")
+	time.Sleep(1 * time.Second)
+
+	// 2
+
+	request2 := kafkaapi.ProduceRequest{
+		Header: builder.NewRequestHeaderBuilder().
+			BuildProduceRequestHeader(correlationId),
+		Body: builder.NewProduceRequestBuilder().
+			AddRecordBatchToTopicPartition(existingTopic, existingPartition, []string{common.HELLO_MSG1}).
+			Build(),
+	}
+
+	message = kafkaapi.EncodeProduceRequest(&request2)
+	stageLogger.Infof("Sending \"Produce\" (version: %v) request (Correlation id: %v)", request2.Header.ApiVersion, request2.Header.CorrelationId)
+	stageLogger.Debugf("Hexdump of sent \"Produce\" request: \n%v\n", GetFormattedHexdump(message))
+
+	response, err = broker.SendAndReceive(message)
+	if err != nil {
+		stageLogger.Errorf("Failed to send and receive \"Produce\" request: %v", err)
+		return err
+	}
+	stageLogger.Debugf("Hexdump of received \"Produce\" response: \n%v\n", GetFormattedHexdump(response.RawBytes))
+
+	responseHeader, responseBody, err = kafkaapi.DecodeProduceHeaderAndResponse(response.Payload, 11, stageLogger)
+	if err != nil {
+		stageLogger.Errorf("Failed to decode \"Produce\" response header: %v", err)
+		return err
+	}
+
+	expectedResponse = builder.NewProduceResponseBuilder().
+		AddTopicPartitionResponse(existingTopic, existingPartition, 0).
+		Build(correlationId)
+
+	headerAssertion = assertions.NewResponseHeaderAssertion(*responseHeader, expectedResponse.Header, stageLogger)
+	err = headerAssertion.AssertHeader([]string{"CorrelationId"}).Run()
+	if err != nil {
+		return err
+	}
+
+	bodyAssertion = assertions.NewProduceResponseAssertion(*responseBody, expectedResponse.Body, stageLogger)
+	// "LogAppendTimeMs" is not -1 if it's not latest message
+	err = bodyAssertion.AssertBody([]string{"ThrottleTimeMs"}).
+		AssertTopics([]string{"Name"}, []string{"ErrorCode", "Index", "BaseOffset", "LogStartOffset"}).
+		Run()
+	if err != nil {
+		return err
+	}
+
+	// Validate RecordBatch in log file
+	expectedBatch = buildExpectedRecordBatchForStageP4()
 	err = validateRecordBatchInLogFile(existingTopic, existingPartition, expectedBatch, stageLogger)
 	if err != nil {
 		return err
