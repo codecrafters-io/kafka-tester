e0f1b37889cccd4da732877e6a0f69e7c30ec142
Ryan <ryan-gg@outlook.com>
2025-07-17 21:46:11 +0530

    feat: enhance Kafka request builders with new interfaces and methods
    - Added methods for adding topic partition responses in ProduceResponseBuilderI, improving flexibility in response construction.
    - Introduced RecordBuilderI and RecordBatchBuilderI interfaces with various methods for building records and record batches, enhancing the Kafka message production capabilities.
    - These additions streamline the process of constructing complex Kafka messages, facilitating better integration and usage in the application.

--- protocol/builder/interface.go
+++ protocol/builder/interface.go
@@ -24,6 +24,8 @@ type KafkaRequestI interface {
 // }
 
 type ProduceResponseBuilderI interface {
+	AddTopicPartitionResponse(topicName string, partitionIndex int32, errorCode int16) *ProduceResponseBuilder
+	AddTopicPartitionResponseWithBaseOffset(topicName string, partitionIndex int32, errorCode int16, baseOffset int64) *ProduceResponseBuilder
 	Build(correlationId int32) kafkaapi.ProduceResponse
 }
 
@@ -45,5 +47,38 @@ type ApiVersionsRequestBuilderI interface {
 
 type ProduceRequestBuilderI interface {
 	AddRecordBatchToTopicPartition(topicName string, partitionIndex int32, messages []string) ProduceRequestBuilderI
+	WithBaseSequence(baseSequence int32) ProduceRequestBuilderI
 	Build() kafkaapi.ProduceRequestBody
+}
+
+type RecordBuilderI interface {
+	WithAttributes(attributes int8) RecordBuilderI
+	WithTimestampDelta(timestampDelta int64) RecordBuilderI
+	WithOffsetDelta(offsetDelta int32) RecordBuilderI
+	WithKey(key []byte) RecordBuilderI
+	WithStringKey(key string) RecordBuilderI
+	WithValue(value []byte) RecordBuilderI
+	WithStringValue(value string) RecordBuilderI
+	WithHeader(key string, value []byte) RecordBuilderI
+	WithStringHeader(key string, value string) RecordBuilderI
+	Build() kafkaapi.Record
+}
+
+type RecordBatchBuilderI interface {
+	WithBaseOffset(baseOffset int64) RecordBatchBuilderI
+	WithPartitionLeaderEpoch(epoch int32) RecordBatchBuilderI
+	WithAttributes(attributes int16) RecordBatchBuilderI
+	WithTimestamps(firstTimestamp, maxTimestamp int64) RecordBatchBuilderI
+	WithFirstTimestamp(timestamp int64) RecordBatchBuilderI
+	WithMaxTimestamp(timestamp int64) RecordBatchBuilderI
+	WithProducer(producerId int64, producerEpoch int16) RecordBatchBuilderI
+	WithProducerId(producerId int64) RecordBatchBuilderI
+	WithProducerEpoch(epoch int16) RecordBatchBuilderI
+	WithBaseSequence(sequence int32) RecordBatchBuilderI
+	AddRecord(record kafkaapi.Record) RecordBatchBuilderI
+	AddRecordFromBuilder(recordBuilder RecordBuilderI) RecordBatchBuilderI
+	AddSimpleRecord(value string) RecordBatchBuilderI
+	AddRecordWithKeyValue(key, value string) RecordBatchBuilderI
+	AddRecords(values []string) RecordBatchBuilderI
+	Build() kafkaapi.RecordBatch
 }
