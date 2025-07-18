504cfd94321da85d272e5a04d71cdf497d81a4c1
Ryan <ryan-gg@outlook.com>
2025-07-18 14:49:48 +0530

    feat: add ApiVersionsResponseBuilder for response creation
    Introduce ApiVersionsResponseBuilder to simplify building API Versions
    responses with configurable version, error code, throttle time, and supported
    API keys. This builder improves code clarity and consistency when creating
    ApiVersionsResponse objects.

---
+++ protocol/builder/api_versions_response_builder.go
@@ -0,0 +1,53 @@
+package builder
+
+import kafkaapi "github.com/codecrafters-io/kafka-tester/protocol/api"
+
+type ApiVersionsResponseBuilder struct {
+	version        int16
+	errorCode      int16
+	apiKeys        []kafkaapi.ApiKeyVersionSupport
+	throttleTimeMs int32
+}
+
+func NewApiVersionsResponseBuilder() *ApiVersionsResponseBuilder {
+	return &ApiVersionsResponseBuilder{
+		version:        3,
+		errorCode:      0,
+		throttleTimeMs: 0,
+		apiKeys:        []kafkaapi.ApiKeyVersionSupport{},
+	}
+}
+
+func (b *ApiVersionsResponseBuilder) WithVersion(version int16) *ApiVersionsResponseBuilder {
+	b.version = version
+	return b
+}
+
+func (b *ApiVersionsResponseBuilder) WithErrorCode(errorCode int16) *ApiVersionsResponseBuilder {
+	b.errorCode = errorCode
+	return b
+}
+
+func (b *ApiVersionsResponseBuilder) WithThrottleTimeMs(throttleTimeMs int32) *ApiVersionsResponseBuilder {
+	b.throttleTimeMs = throttleTimeMs
+	return b
+}
+
+func (b *ApiVersionsResponseBuilder) AddApiKeyVersionSupport(apiKey int16, minVersion int16, maxVersion int16) *ApiVersionsResponseBuilder {
+	b.apiKeys = append(b.apiKeys, kafkaapi.ApiKeyVersionSupport{
+		Version:    b.version,
+		ApiKey:     apiKey,
+		MinVersion: minVersion,
+		MaxVersion: maxVersion,
+	})
+	return b
+}
+
+func (b *ApiVersionsResponseBuilder) Build(correlationId int32) kafkaapi.ApiVersionsResponse {
+	return kafkaapi.ApiVersionsResponse{
+		Version:        b.version,
+		ErrorCode:      b.errorCode,
+		ApiKeys:        b.apiKeys,
+		ThrottleTimeMs: b.throttleTimeMs,
+	}
+}
