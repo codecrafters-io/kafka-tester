ad1a8298f343384b9bd9edad2ed3846f0357cd31
Ryan <ryan-gg@outlook.com>
2025-07-22 11:10:18 +0530

    feat: add initial testProduce1 function to validate Kafka API versions response

---
+++ internal/stage_p1.go
@@ -0,0 +1,68 @@
+package internal
+
+import (
+	"github.com/codecrafters-io/kafka-tester/internal/assertions"
+	"github.com/codecrafters-io/kafka-tester/internal/kafka_executable"
+	kafkaapi "github.com/codecrafters-io/kafka-tester/protocol/api"
+	"github.com/codecrafters-io/kafka-tester/protocol/builder"
+	"github.com/codecrafters-io/kafka-tester/protocol/kafka_client"
+	"github.com/codecrafters-io/kafka-tester/protocol/serializer"
+	"github.com/codecrafters-io/tester-utils/logger"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+func testProduce1(stageHarness *test_case_harness.TestCaseHarness) error {
+	b := kafka_executable.NewKafkaExecutable(stageHarness)
+	err := serializer.GenerateLogDirs(logger.GetQuietLogger(""), false)
+	if err != nil {
+		return err
+	}
+
+	stageLogger := stageHarness.Logger
+	if err := b.Run(); err != nil {
+		return err
+	}
+
+	correlationId := getRandomCorrelationId()
+
+	client := kafka_client.NewClient("localhost:9092")
+	if err := client.ConnectWithRetries(b, stageLogger); err != nil {
+		return err
+	}
+	defer func(client *kafka_client.Client) {
+		_ = client.Close()
+	}(client)
+
+	request := kafkaapi.ApiVersionsRequest{
+		Header: builder.NewRequestHeaderBuilder().BuildApiVersionsRequestHeader(correlationId),
+		Body:   builder.NewApiVersionsRequestBuilder().Build(),
+	}
+
+	response, err := client.SendAndReceive(request, stageLogger)
+	if err != nil {
+		return err
+	}
+
+	responseHeader, responseBody, err := kafkaapi.DecodeApiVersionsHeaderAndResponse(response.Payload, 3, stageLogger)
+	if err != nil {
+		return err
+	}
+
+	expectedResponseHeader := kafkaapi.ResponseHeader{
+		CorrelationId: correlationId,
+	}
+	if err = assertions.NewResponseHeaderAssertion(*responseHeader, expectedResponseHeader).Evaluate([]string{"CorrelationId"}, stageLogger); err != nil {
+		return err
+	}
+
+	expectedApiVersionResponse := builder.NewApiVersionsResponseBuilder().
+		AddApiKeyEntry(0, 0, 11).
+		AddApiKeyEntry(18, 0, 4).
+		Build(correlationId)
+
+	if err = assertions.NewApiVersionsResponseAssertion(*responseBody, expectedApiVersionResponse.Body).Run(stageLogger); err != nil {
+		return err
+	}
+
+	return nil
+}
