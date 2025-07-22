68d63848615df80f0cccae5705cbea7b052217ed
Ryan <ryan-gg@outlook.com>
2025-07-22 13:53:59 +0530

    refactor(produce): remove EncodeProduceRequest and DecodeProduceHeader

--- protocol/api/produce.go
+++ protocol/api/produce.go
@@ -2,42 +2,13 @@ package kafkaapi
 
 import (
 	"github.com/codecrafters-io/kafka-tester/protocol/decoder"
-	"github.com/codecrafters-io/kafka-tester/protocol/encoder"
 
 	"github.com/codecrafters-io/kafka-tester/protocol/errors"
 	"github.com/codecrafters-io/tester-utils/logger"
 )
 
-func EncodeProduceRequest(request *ProduceRequest) []byte {
-	encoder := encoder.Encoder{}
-	// bytes.Buffer{}
-	encoder.Init(make([]byte, 4096))
-
-	request.Header.EncodeV2(&encoder)
-	request.Body.Encode(&encoder)
-	message := encoder.PackMessage()
-
-	return message
-}
-
-func DecodeProduceHeader(response []byte, version int16, logger *logger.Logger) (*ResponseHeader, error) {
-	decoder := decoder.Decoder{}
-	decoder.Init(response)
-	logger.UpdateLastSecondaryPrefix("Decoder")
-	defer logger.ResetSecondaryPrefixes()
-
-	responseHeader := ResponseHeader{}
-	logger.Debugf("- .ResponseHeader")
-	if err := responseHeader.DecodeV1(&decoder, logger, 1); err != nil {
-		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
-			detailedError := decodingErr.WithAddedContext("Response Header").WithAddedContext("Produce Response v11")
-			return nil, decoder.FormatDetailedError(detailedError.Error())
-		}
-		return nil, err
-	}
-
-	return &responseHeader, nil
-}
+// TODO: Use an universal DecodeHeaderAndResponse function
+// Accept an API name, and based on it switch the decoder
 
 func DecodeProduceHeaderAndResponse(response []byte, version int16, logger *logger.Logger) (*ResponseHeader, *ProduceResponseBody, error) {
 	decoder := decoder.Decoder{}
