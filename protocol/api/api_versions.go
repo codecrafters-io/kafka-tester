cd87132192a7132ffbf9e569106d7548d3715e96
Ryan <ryan-gg@outlook.com>
2025-07-23 15:57:07 +0530

    fix(api): update request header encoding to use Encode method

--- protocol/api/api_versions.go
+++ protocol/api/api_versions.go
@@ -3,10 +3,41 @@ package kafkaapi
 import (
 	"github.com/codecrafters-io/kafka-tester/protocol/api/headers"
 	"github.com/codecrafters-io/kafka-tester/protocol/decoder"
+	"github.com/codecrafters-io/kafka-tester/protocol/encoder"
 
 	"github.com/codecrafters-io/kafka-tester/protocol/errors"
 	"github.com/codecrafters-io/tester-utils/logger"
 )
+
+func EncodeApiVersionsRequest(request *ApiVersionsRequest) []byte {
+	encoder := encoder.Encoder{}
+	encoder.Init(make([]byte, 4096))
+
+	request.Header.Encode(&encoder)
+	request.Body.Encode(&encoder)
+	messageBytes := encoder.PackMessage()
+
+	return messageBytes
+}
+
+func DecodeApiVersionsHeader(response []byte, version int16, logger *logger.Logger) (*ResponseHeader, error) {
+	decoder := decoder.Decoder{}
+	decoder.Init(response)
+	logger.UpdateLastSecondaryPrefix("Decoder")
+	defer logger.ResetSecondaryPrefixes()
+
+	responseHeader := ResponseHeader{}
+	logger.Debugf("- .ResponseHeader")
+	// APIVersions always uses Header v0
+	if err := responseHeader.DecodeV0(&decoder, logger, 1); err != nil {
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return nil, decodingErr.WithAddedContext("Response Header").WithAddedContext("ApiVersions v3")
+		}
+		return nil, err
+	}
+
+	return &responseHeader, nil
+}
 
 // DecodeApiVersionsHeaderAndResponse decodes the header and response
 // If an error is encountered while decoding, the returned objects are nil
