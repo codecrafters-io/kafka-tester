f3f7ed0865001a6a5a691bade16153ebf0307f17
Ryan <ryan-gg@outlook.com>
2025-07-23 16:06:38 +0530

    feat: add ResponseHeaderBuilder to construct response headers with correlation ID validation

---
+++ protocol/builder/response_header_builder.go
@@ -0,0 +1,30 @@
+package builder
+
+import (
+	headers "github.com/codecrafters-io/kafka-tester/protocol/api/headers"
+)
+
+type ResponseHeaderBuilder struct {
+	correlationId int32
+}
+
+func NewResponseHeaderBuilder() *ResponseHeaderBuilder {
+	return &ResponseHeaderBuilder{
+		correlationId: -1,
+	}
+}
+
+func (rb *ResponseHeaderBuilder) WithCorrelationId(correlationId int32) *ResponseHeaderBuilder {
+	rb.correlationId = correlationId
+	return rb
+}
+
+func (rb *ResponseHeaderBuilder) Build() headers.ResponseHeader {
+	if rb.correlationId == 0 {
+		panic("CodeCrafters Internal Error: correlationId can't be -1 for building a response header")
+	}
+
+	return headers.ResponseHeader{
+		CorrelationId: rb.correlationId,
+	}
+}
