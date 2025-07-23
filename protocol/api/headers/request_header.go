135d7309e818e516c803741d9cf0daccbf6ad9df
Ryan <ryan-gg@outlook.com>
2025-07-24 01:42:21 +0530

    feat(protocol): add request and response header structs with encoding and decoding

---
+++ protocol/api/headers/request_header.go
@@ -0,0 +1,26 @@
+package headers
+
+import (
+	"github.com/codecrafters-io/kafka-tester/protocol/encoder"
+)
+
+// RequestHeader defines the header for a Kafka request
+type RequestHeader struct {
+	// ApiKey defines the API key for the request
+	ApiKey int16
+	// ApiVersion defines the API version for the request
+	ApiVersion int16
+	// CorrelationId defines the correlation ID for the request
+	CorrelationId int32
+	// ClientId defines the client ID for the request
+	ClientId string
+}
+
+// Encode v2
+func (h RequestHeader) Encode(enc *encoder.Encoder) {
+	enc.PutInt16(h.ApiKey)
+	enc.PutInt16(h.ApiVersion)
+	enc.PutInt32(h.CorrelationId)
+	enc.PutString(h.ClientId)
+	enc.PutEmptyTaggedFieldArray()
+}
---
+++ protocol/api/headers/response_header.go
@@ -0,0 +1,62 @@
+package headers
+
+import (
+	"fmt"
+
+	"github.com/codecrafters-io/kafka-tester/protocol"
+	"github.com/codecrafters-io/kafka-tester/protocol/decoder"
+	"github.com/codecrafters-io/kafka-tester/protocol/errors"
+	"github.com/codecrafters-io/tester-utils/logger"
+)
+
+// ResponseHeader defines the header for a Kafka response
+type ResponseHeader struct {
+	Version       int
+	CorrelationId int32
+}
+
+func (h *ResponseHeader) Decode(decoder *decoder.Decoder, logger *logger.Logger, indentation int) error {
+	if h.Version > 1 {
+		panic(fmt.Sprintf("CodeCrafters Internal Error: Unsupported response header version: %d", h.Version))
+	}
+
+	switch h.Version {
+	case 0:
+		return h.decodeV0(decoder, logger, indentation)
+	case 1:
+		return h.decodeV1(decoder, logger, indentation)
+	}
+	return nil
+}
+
+func (h *ResponseHeader) decodeV0(decoder *decoder.Decoder, logger *logger.Logger, indentation int) error {
+	correlation_id, err := decoder.GetInt32()
+	if err != nil {
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("correlation_id")
+		}
+		return err
+	}
+	h.CorrelationId = correlation_id
+	protocol.LogWithIndentation(logger, indentation, "- .correlation_id (%d)", correlation_id)
+
+	return nil
+}
+
+func (h *ResponseHeader) decodeV1(decoder *decoder.Decoder, logger *logger.Logger, indentation int) error {
+	err := h.decodeV0(decoder, logger, indentation)
+	if err != nil {
+		return err
+	}
+
+	_, err = decoder.GetEmptyTaggedFieldArray()
+	if err != nil {
+		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
+			return decodingErr.WithAddedContext("TAG_BUFFER")
+		}
+		return err
+	}
+	protocol.LogWithIndentation(logger, indentation, "- .TAG_BUFFER")
+
+	return nil
+}
