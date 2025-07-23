45f16e27650f8ca77557dedc28b4d1a484bf5bdb
Ryan <ryan-gg@outlook.com>
2025-07-24 02:04:41 +0530

    refactor(protocol): simplify DescribeTopicPartitionsRequest encoding by removing encoder dependency and using direct byte slice concatenation

--- protocol/api/describe_topic_partitions.go
+++ protocol/api/describe_topic_partitions.go
@@ -3,19 +3,12 @@ package kafkaapi
 import (
 	"github.com/codecrafters-io/kafka-tester/protocol/api/headers"
 	"github.com/codecrafters-io/kafka-tester/protocol/decoder"
-	"github.com/codecrafters-io/kafka-tester/protocol/encoder"
 	"github.com/codecrafters-io/kafka-tester/protocol/errors"
 	"github.com/codecrafters-io/tester-utils/logger"
 )
 
 func EncodeDescribeTopicPartitionsRequest(request *DescribeTopicPartitionsRequest) []byte {
-	encoder := encoder.Encoder{}
-	encoder.Init(make([]byte, 4096))
-
-	request.Encode(&encoder)
-	messageBytes := encoder.PackMessage()
-
-	return messageBytes
+	return append(request.Header.Encode(), request.Body.Encode()...)
 }
 
 // DecodeDescribeTopicPartitionsHeaderAndResponse decodes the header and response
