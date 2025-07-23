82056543ba3780eee9fc2604ceeb850cfd71e919
Ryan <ryan-gg@outlook.com>
2025-07-23 11:52:56 +0530

    fix: re-enable partition field checks in response assertion

--- internal/stage_dtp2.go
+++ internal/stage_dtp2.go
@@ -80,6 +80,5 @@ func testDTPartitionWithUnknownTopic(stageHarness *test_case_harness.TestCaseHar
 	}
 
 	return assertions.NewDescribeTopicPartitionsResponseAssertion(*responseBody, expectedDescribeTopicPartitionsResponse).
-		SkipPartitionFields().
 		Run(stageLogger)
 }
