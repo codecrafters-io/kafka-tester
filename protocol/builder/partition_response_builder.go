d8b60b5dc87e6f96021aa63fcfe0a03032281d7d
Ryan <ryan-gg@outlook.com>
2025-07-24 16:48:50 +0530

    feat(builder): add index field and enforce its setting before build

--- protocol/builder/partition_response_builder.go
+++ protocol/builder/partition_response_builder.go
@@ -5,6 +5,7 @@ import (
 )
 
 type PartitionResponseBuilder struct {
+	index           int32
 	errorCode       int16
 	baseOffset      int64
 	logAppendTimeMs int64
@@ -15,6 +16,7 @@ type PartitionResponseBuilder struct {
 
 func NewPartitionResponseBuilder() *PartitionResponseBuilder {
 	return &PartitionResponseBuilder{
+		index:           -1,
 		errorCode:       0,
 		baseOffset:      0,
 		logAppendTimeMs: -1,
@@ -22,6 +24,11 @@ func NewPartitionResponseBuilder() *PartitionResponseBuilder {
 		recordErrors:    []kafkaapi.RecordError{},
 		errorMessage:    nil,
 	}
+}
+
+func (b *PartitionResponseBuilder) WithIndex(index int32) *PartitionResponseBuilder {
+	b.index = index
+	return b
 }
 
 func (b *PartitionResponseBuilder) WithError(errorCode int16) *PartitionResponseBuilder {
@@ -58,9 +65,13 @@ func (b *PartitionResponseBuilder) WithErrorMessage(errorMessage string) *Partit
 	return b
 }
 
-func (b *PartitionResponseBuilder) Build(partitionIndex int32) kafkaapi.ProducePartitionResponse {
+func (b *PartitionResponseBuilder) Build() kafkaapi.ProducePartitionResponse {
+	if b.index == -1 {
+		panic("CodeCrafters Internal Error: index must be set before building PartitionResponse")
+	}
+
 	return kafkaapi.ProducePartitionResponse{
-		Index:           partitionIndex,
+		Index:           b.index,
 		ErrorCode:       b.errorCode,
 		BaseOffset:      b.baseOffset,
 		LogAppendTimeMs: b.logAppendTimeMs,
