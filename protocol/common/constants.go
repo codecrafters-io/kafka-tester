371e1f49a003e2941fc00c9065a59d45e450221c
Ryan <ryan-gg@outlook.com>
2025-07-24 22:11:32 +0530

    feat: add 4th topic to support Produce tests
    2 each for Fetch, Produce
    Of which 1 has single partition, and the other has multiple partitions.

--- protocol/common/constants.go
+++ protocol/common/constants.go
@@ -2,7 +2,7 @@ package common
 
 import (
 	"fmt"
-	"sort"
+	"slices"
 
 	"github.com/codecrafters-io/tester-utils/random"
 )
@@ -21,33 +21,28 @@ const (
 
 var (
 	// Topic names and UUIDs - initialized in init()
-	TOPIC1_NAME       string
-	TOPIC2_NAME       string
-	TOPIC3_NAME       string
-	TOPIC1_UUID       string
-	TOPIC2_UUID       string
-	TOPIC3_UUID       string
-	TOPICX_UUID       string
-	TOPIC_UNKOWN_NAME string
-	TOPIC_UNKOWN_UUID string
+	TOPIC1_NAME, TOPIC2_NAME, TOPIC3_NAME, TOPIC4_NAME string
+	TOPIC1_UUID, TOPIC2_UUID, TOPIC3_UUID, TOPIC4_UUID string
+	TOPICX_UUID                                        string
+	TOPIC_UNKOWN_NAME, TOPIC_UNKOWN_UUID               string
 
-	MESSAGE1 string
-	MESSAGE2 string
-	MESSAGE3 string
+	MESSAGE1, MESSAGE2, MESSAGE3 string
 )
 
 func init() {
 	random.Init()
 
 	all_topic_names := []string{"foo", "bar", "baz", "qux", "quz", "pax", "paz", "saz"}
-	topic_names := GetSortedValues(random.RandomElementsFromArray(all_topic_names, 4))
+	topic_names := getSortedValues(random.RandomElementsFromArray(all_topic_names, 4))
 	TOPIC1_NAME = topic_names[0]
 	TOPIC2_NAME = topic_names[1]
 	TOPIC3_NAME = topic_names[2]
-	random_topic_uuids := random.RandomInts(10, 100, 3)
+	TOPIC4_NAME = topic_names[3]
+	random_topic_uuids := random.RandomInts(10, 100, 4)
 	TOPIC1_UUID = fmt.Sprintf("00000000-0000-4000-8000-0000000000%02d", random_topic_uuids[0])
 	TOPIC2_UUID = fmt.Sprintf("00000000-0000-4000-8000-0000000000%02d", random_topic_uuids[1])
 	TOPIC3_UUID = fmt.Sprintf("00000000-0000-4000-8000-0000000000%02d", random_topic_uuids[2])
+	TOPIC4_UUID = fmt.Sprintf("00000000-0000-4000-8000-0000000000%02d", random_topic_uuids[3])
 	TOPICX_UUID = fmt.Sprintf("00000000-0000-0000-0000-00000000%04d", random.RandomInt(1000, 9999)) // Unknown topic used in requests
 
 	TOPIC_UNKOWN_NAME = fmt.Sprintf("unknown-topic-%s", topic_names[3])
@@ -60,9 +55,7 @@ func init() {
 	MESSAGE3 = messages[2]
 }
 
-func GetSortedValues[T string](values []T) []T {
-	sort.Slice(values, func(i, j int) bool {
-		return values[i] < values[j]
-	})
+func getSortedValues[T string](values []T) []T {
+	slices.Sort(values)
 	return values
 }
