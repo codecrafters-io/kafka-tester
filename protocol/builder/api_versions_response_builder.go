72f7743527276c2fa554da64ab0f31694fa5747b
Ryan <ryan-gg@outlook.com>
2025-07-25 02:11:46 +0530

    feat: add correlationId to ApiVersionsResponseBuilder and enforce its use

--- protocol/builder/api_versions_response_builder.go
+++ protocol/builder/api_versions_response_builder.go
@@ -7,6 +7,7 @@ import (
 
 type ApiVersionsResponseBuilder struct {
 	version        int16
+	correlationId  int32
 	errorCode      int16
 	apiKeys        []kafkaapi.ApiKeyEntry
 	throttleTimeMs int32
@@ -15,10 +16,16 @@ type ApiVersionsResponseBuilder struct {
 func NewApiVersionsResponseBuilder() *ApiVersionsResponseBuilder {
 	return &ApiVersionsResponseBuilder{
 		version:        3,
+		correlationId:  -1,
 		errorCode:      0,
 		throttleTimeMs: 0,
 		apiKeys:        []kafkaapi.ApiKeyEntry{},
 	}
+}
+
+func (b *ApiVersionsResponseBuilder) WithCorrelationId(correlationId int32) *ApiVersionsResponseBuilder {
+	b.correlationId = correlationId
+	return b
 }
 
 func (b *ApiVersionsResponseBuilder) WithErrorCode(errorCode int16) *ApiVersionsResponseBuilder {
@@ -41,11 +48,16 @@ func (b *ApiVersionsResponseBuilder) AddApiKeyEntry(apiKey int16, minVersion int
 	return b
 }
 
-func (b *ApiVersionsResponseBuilder) Build(correlationId int32) kafkaapi.ApiVersionsResponse {
+func (b *ApiVersionsResponseBuilder) Build() kafkaapi.ApiVersionsResponse {
+	if b.correlationId == -1 {
+		panic("CodeCrafters Internal Error: CorrelationId is required")
+	}
+
 	return kafkaapi.ApiVersionsResponse{
 		// TODO: Add ResponseHeaderBuilder
 		Header: headers.ResponseHeader{
-			CorrelationId: correlationId,
+			Version:       0,
+			CorrelationId: b.correlationId,
 		},
 		Body: kafkaapi.ApiVersionsResponseBody{
 			Version:        b.version,
@@ -53,5 +65,14 @@ func (b *ApiVersionsResponseBuilder) Build(correlationId int32) kafkaapi.ApiVers
 			ApiKeys:        b.apiKeys,
 			ThrottleTimeMs: b.throttleTimeMs,
 		},
+	}
+}
+
+func (b *ApiVersionsResponseBuilder) BuildEmpty() kafkaapi.ApiVersionsResponse {
+	return kafkaapi.ApiVersionsResponse{
+		Header: headers.ResponseHeader{
+			Version: 0,
+		},
+		Body: kafkaapi.ApiVersionsResponseBody{Version: 3},
 	}
 }
