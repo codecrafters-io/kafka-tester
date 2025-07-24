1a249c2798e7c9d9480b343255f1ece1db51bda0
Ryan <ryan-gg@outlook.com>
2025-07-25 01:22:45 +0530

    refactor: simplify topicData structure to use slices for easier management and improve code clarity

--- protocol/builder/produce_request_builder.go
+++ protocol/builder/produce_request_builder.go
@@ -5,9 +5,8 @@ import (
 )
 
 type ProduceRequestBuilder struct {
-	correlationId int32
-	// topicName -> partitionIndex -> recordBatches
-	topicData       map[string]map[int32]kafkaapi.RecordBatches
+	correlationId   int32
+	topicData       []kafkaapi.ProduceTopicData
 	transactionalID *string
 	acks            int16
 	timeoutMs       int32
@@ -16,7 +15,7 @@ type ProduceRequestBuilder struct {
 func NewProduceRequestBuilder() *ProduceRequestBuilder {
 	return &ProduceRequestBuilder{
 		correlationId:   -1,
-		topicData:       make(map[string]map[int32]kafkaapi.RecordBatches),
+		topicData:       make([]kafkaapi.ProduceTopicData, 0),
 		transactionalID: nil,
 		acks:            1,
 		timeoutMs:       0,
@@ -29,11 +28,17 @@ func (b *ProduceRequestBuilder) WithCorrelationId(correlationId int32) *ProduceR
 }
 
 func (b *ProduceRequestBuilder) AddRecordBatch(topicName string, partitionIndex int32, recordBatch kafkaapi.RecordBatch) *ProduceRequestBuilder {
-	if b.topicData[topicName] == nil {
-		b.topicData[topicName] = make(map[int32]kafkaapi.RecordBatches)
+	topicData := kafkaapi.ProduceTopicData{
+		Name: topicName,
+		Partitions: []kafkaapi.ProducePartitionData{
+			{
+				Index:         partitionIndex,
+				RecordBatches: []kafkaapi.RecordBatch{recordBatch},
+			},
+		},
 	}
 
-	b.topicData[topicName][partitionIndex] = append(b.topicData[topicName][partitionIndex], recordBatch)
+	b.topicData = append(b.topicData, topicData)
 	return b
 }
 
@@ -42,30 +47,11 @@ func (b *ProduceRequestBuilder) Build() kafkaapi.ProduceRequest {
 		panic("CodeCrafters Internal Error: At least one topic with partitions and record batches is required")
 	}
 
-	// Convert topicData map to slice
-	topicData := make([]kafkaapi.ProduceTopicData, 0, len(b.topicData))
-	for topicName := range b.topicData {
-		partitions := b.topicData[topicName]
-		// Convert partitions map to slice for this topic
-		partitionData := make([]kafkaapi.ProducePartitionData, 0, len(partitions))
-		for partitionIndex, recordBatches := range partitions {
-			partitionData = append(partitionData, kafkaapi.ProducePartitionData{
-				Index:         partitionIndex,
-				RecordBatches: recordBatches,
-			})
-		}
-
-		topicData = append(topicData, kafkaapi.ProduceTopicData{
-			Name:       topicName,
-			Partitions: partitionData,
-		})
-	}
-
 	requestBody := kafkaapi.ProduceRequestBody{
 		TransactionalID: b.transactionalID,
 		Acks:            b.acks,
 		TimeoutMs:       b.timeoutMs,
-		Topics:          topicData,
+		Topics:          b.topicData,
 	}
 
 	return kafkaapi.ProduceRequest{
