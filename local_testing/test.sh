d4e1030d9f8b7f9887593da220d78015dd78c19c
Ryan <ryan-gg@outlook.com>
2025-07-25 21:49:47 +0530

    chore: update local testing scripts and Dockerfile for Kafka setup

--- local_testing/test.sh
+++ local_testing/test.sh
@@ -10,6 +10,6 @@ docker build -t local-kafka-tester -f local_testing/Dockerfile .
 # Run make test
 # docker run --rm -it -v $(pwd):/app local-git-tester make test
 # Generate fixtures
-# docker run --rm -it -e CODECRAFTERS_RECORD_FIXTURES=true -v $(pwd):/app local-git-tester make test
-# Run test_all
-docker run --rm -it -v $(pwd):/app local-kafka-tester make test_all
+# docker run --rm -it -e CODECRAFTERS_RECORD_FIXTURES=true -v $(pwd):/app local-kafka-tester make test
+# Run test_all/
+docker run --rm -it -v $(pwd):/app local-kafka-tester make test
