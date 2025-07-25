f5ff89bdf84680de57461c155d200e70c86c6251
Ryan <ryan-gg@outlook.com>
2025-07-26 02:28:50 +0530

    chore: setup local testing

--- local_testing/test.sh
+++ local_testing/test.sh
@@ -7,9 +7,6 @@ cd "$(dirname "$0")/.."
 
 # Build and run
 docker build -t local-kafka-tester -f local_testing/Dockerfile .
-# Run make test
-# docker run --rm -it -v $(pwd):/app local-git-tester make test
 # Generate fixtures
 # docker run --rm -it -e CODECRAFTERS_RECORD_FIXTURES=true -v $(pwd):/app local-kafka-tester make test
-# Run test_all/
 docker run --rm -it -v $(pwd):/app local-kafka-tester make test
