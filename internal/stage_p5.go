3704be2a53cf1960cc22e5a64cff5a75e6bf0ce0
Ryan <ryan-gg@outlook.com>
2025-07-25 21:49:01 +0530

    feat: add testProduce5 to validate multi-batch produce requests and responses

---
+++ internal/stage_p5.go
@@ -0,0 +1,82 @@
+package internal
+
+import (
+	"github.com/codecrafters-io/kafka-tester/internal/assertions"
+	"github.com/codecrafters-io/kafka-tester/internal/kafka_executable"
+	kafkaapi "github.com/codecrafters-io/kafka-tester/protocol/api"
+	"github.com/codecrafters-io/kafka-tester/protocol/builder"
+	"github.com/codecrafters-io/kafka-tester/protocol/common"
+	"github.com/codecrafters-io/kafka-tester/protocol/kafka_client"
+	"github.com/codecrafters-io/kafka-tester/protocol/serializer"
+	"github.com/codecrafters-io/tester-utils/random"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+func testProduce5(stageHarness *test_case_harness.TestCaseHarness) error {
+	b := kafka_executable.NewKafkaExecutable(stageHarness)
+	stageLogger := stageHarness.Logger
+	err := serializer.GenerateLogDirs(stageLogger, false)
+	if err != nil {
+		return err
+	}
+
+	if err := b.Run(); err != nil {
+		return err
+	}
+
+	correlationId := getRandomCorrelationId()
+
+	client := kafka_client.NewClient("localhost:9092")
+	if err := client.ConnectWithRetries(b, stageLogger); err != nil {
+		return err
+	}
+	defer client.Close()
+
+	topic := common.TOPIC4_NAME
+	partition := int32(random.RandomInt(0, 3))
+
+	recordBatch1 := builder.NewRecordBatchBuilder().
+		AddStringRecord(common.MESSAGE1).
+		Build()
+
+	recordBatch2 := builder.NewRecordBatchBuilder().
+		AddStringRecord(common.MESSAGE2).
+		Build()
+
+	recordBatch3 := builder.NewRecordBatchBuilder().
+		AddStringRecord(common.MESSAGE3).
+		Build()
+
+	request := builder.NewProduceRequestBuilder().
+		AddRecordBatch(topic, partition, recordBatch1).
+		AddRecordBatch(topic, partition, recordBatch2).
+		AddRecordBatch(topic, partition, recordBatch3).
+		WithCorrelationId(correlationId).
+		Build()
+
+	rawResponse, err := client.SendAndReceive(request, stageLogger)
+	if err != nil {
+		return err
+	}
+
+	actualResponse := builder.NewEmptyProduceResponse()
+	if err := actualResponse.Decode(rawResponse.Payload, stageLogger); err != nil {
+		return err
+	}
+
+	expectedResponse := builder.NewProduceResponseBuilder().
+		AddSuccessPartitionResponse(topic, partition).
+		WithCorrelationId(correlationId).
+		Build()
+
+	if err = assertions.NewProduceResponseAssertion(actualResponse, expectedResponse, stageLogger).Run(stageLogger); err != nil {
+		return err
+	}
+
+	topicPartitionLogAssertion := assertions.NewTopicPartitionLogAssertion(topic, partition, []kafkaapi.RecordBatch{request.Body.Topics[0].Partitions[0].RecordBatches[0], request.Body.Topics[0].Partitions[0].RecordBatches[1], request.Body.Topics[0].Partitions[0].RecordBatches[2]}, stageLogger)
+	if err = topicPartitionLogAssertion.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
