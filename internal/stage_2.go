fe5d6211b57d647c4c0497bf56e4ad1c14353ec7
Ryan <ryan-gg@outlook.com>
2025-07-26 14:29:37 +0530

    fix: normalize log message casing for response_header across stages

--- internal/stage_2.go
+++ internal/stage_2.go
@@ -75,7 +75,7 @@ func testHardcodedCorrelationId(stageHarness *test_case_harness.TestCaseHarness)
 	}
 	protocol.LogWithIndentation(stageLogger, 1, "- .message_length (%d)", messageLength)
 
-	stageLogger.Debugf("- .ResponseHeader")
+	stageLogger.Debugf("- .response_header")
 	responseCorrelationId, err := decoder.GetInt32()
 	if err != nil {
 		if decodingErr, ok := err.(*errors.PacketDecodingError); ok {
