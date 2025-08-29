c8d1f04323a9dfd3e2714c02478bd7551cd2301a
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2025-08-29 15:07:00 +0545

    migrate upto stage 4

---
+++ protocol/builder/request_header_builder.go
@@ -0,0 +1,63 @@
+package builder
+
+import (
+	"github.com/codecrafters-io/kafka-tester/protocol/kafkaapi/headers"
+)
+
+type RequestHeaderBuilder struct {
+	apiKey        int16
+	apiVersion    int16
+	correlationId int32
+}
+
+func NewRequestHeaderBuilder() *RequestHeaderBuilder {
+	return &RequestHeaderBuilder{
+		apiKey:        0,
+		apiVersion:    0,
+		correlationId: -1,
+	}
+}
+
+func (b *RequestHeaderBuilder) WithApiKey(apiKey int16) *RequestHeaderBuilder {
+	b.apiKey = apiKey
+	return b
+}
+
+func (b *RequestHeaderBuilder) WithApiVersion(apiVersion int16) *RequestHeaderBuilder {
+	b.apiVersion = apiVersion
+	return b
+}
+
+func (b *RequestHeaderBuilder) WithCorrelationId(correlationId int32) *RequestHeaderBuilder {
+	b.correlationId = correlationId
+	return b
+}
+
+func (b *RequestHeaderBuilder) Build() headers.RequestHeader {
+	if b.correlationId == -1 {
+		panic("CodeCrafters Internal Error: Correlation ID is required")
+	}
+
+	return headers.RequestHeader{
+		ApiKey:        b.apiKey,
+		ApiVersion:    b.apiVersion,
+		CorrelationId: b.correlationId,
+		ClientId:      "kafka-tester",
+	}
+}
+
+func (b *RequestHeaderBuilder) BuildProduceRequestHeader(correlationId int32) headers.RequestHeader {
+	return b.WithApiKey(0).WithApiVersion(11).WithCorrelationId(correlationId).Build()
+}
+
+func (b *RequestHeaderBuilder) BuildFetchRequestHeader(correlationId int32) headers.RequestHeader {
+	return b.WithApiKey(1).WithApiVersion(16).WithCorrelationId(correlationId).Build()
+}
+
+func (b *RequestHeaderBuilder) BuildApiVersionsRequestHeader(correlationId int32) headers.RequestHeader {
+	return b.WithApiKey(18).WithApiVersion(4).WithCorrelationId(correlationId).Build()
+}
+
+func (b *RequestHeaderBuilder) BuildDescribeTopicPartitionsRequestHeader(correlationId int32) headers.RequestHeader {
+	return b.WithApiKey(75).WithApiVersion(0).WithCorrelationId(correlationId).Build()
+}
