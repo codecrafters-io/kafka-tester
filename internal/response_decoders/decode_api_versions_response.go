1e3bdf9d2ac4705212597e35151d6f4a1c606f9f
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-09-08 17:08:32 -0700

    feat(decoder): add Decoder for primitive Kafka types and compact arrays
    Introduce a new Decoder struct to read primitive Kafka protocol types
    like Int16, Int32, UnsignedVarint, and CompactArrayLength from a byte
    buffer with proper bounds checking and error handling. Implement support
    for consuming tagged fields by skipping unknown tag buffers.
    
    Add common response header decoding for version 0 and a generic helper
    to decode compact arrays with element-specific decode functions.
    
    These changes provide foundational decoding infrastructure needed for
    parsing Kafka protocol responses in a robust and extensible manner.

---
+++ internal/response_decoders/decode_api_versions_response.go
@@ -0,0 +1,84 @@
+package response_decoders
+
+import (
+	"github.com/codecrafters-io/kafka-tester/internal/field_decoder"
+	"github.com/codecrafters-io/kafka-tester/protocol/kafkaapi"
+)
+
+func DecodeApiVersionsResponse(decoder *field_decoder.FieldDecoder) (kafkaapi.ApiVersionsResponse, field_decoder.FieldDecoderError) {
+	decoder.PushPathContext("ApiVersionsResponse")
+	defer decoder.PopPathContext()
+
+	header, err := decodeV0Header(decoder)
+	if err != nil {
+		return kafkaapi.ApiVersionsResponse{}, err
+	}
+
+	body, err := decodeApiVersionsResponseBody(decoder)
+	if err != nil {
+		return kafkaapi.ApiVersionsResponse{}, err
+	}
+
+	return kafkaapi.ApiVersionsResponse{
+		Header: header,
+		Body:   body,
+	}, nil
+}
+
+func decodeApiVersionsResponseBody(decoder *field_decoder.FieldDecoder) (kafkaapi.ApiVersionsResponseBody, field_decoder.FieldDecoderError) {
+	decoder.PushPathContext("Body")
+	defer decoder.PopPathContext()
+
+	errorCode, err := decoder.ReadInt16Field("ErrorCode")
+	if err != nil {
+		return kafkaapi.ApiVersionsResponseBody{}, err
+	}
+
+	apiKeyEntries, err := decodeCompactArray(decoder, decodeApiVersionsResponseApiKeyEntry, "ApiKeys")
+	if err != nil {
+		return kafkaapi.ApiVersionsResponseBody{}, err
+	}
+
+	throttleTimeMs, err := decoder.ReadInt32Field("ThrottleTimeMs")
+	if err != nil {
+		return kafkaapi.ApiVersionsResponseBody{}, err
+	}
+
+	if err := decoder.ConsumeTagBufferField(); err != nil {
+		return kafkaapi.ApiVersionsResponseBody{}, err
+	}
+
+	return kafkaapi.ApiVersionsResponseBody{
+		Version:        4,
+		ErrorCode:      errorCode,
+		ApiKeys:        apiKeyEntries,
+		ThrottleTimeMs: throttleTimeMs,
+	}, nil
+}
+
+func decodeApiVersionsResponseApiKeyEntry(decoder *field_decoder.FieldDecoder) (kafkaapi.ApiKeyEntry, field_decoder.FieldDecoderError) {
+	apiKey, err := decoder.ReadInt16Field("APIKey")
+	if err != nil {
+		return kafkaapi.ApiKeyEntry{}, err
+	}
+
+	minVersion, err := decoder.ReadInt16Field("MinVersion")
+	if err != nil {
+		return kafkaapi.ApiKeyEntry{}, err
+	}
+
+	maxVersion, err := decoder.ReadInt16Field("MaxVersion")
+	if err != nil {
+		return kafkaapi.ApiKeyEntry{}, err
+	}
+
+	if err := decoder.ConsumeTagBufferField(); err != nil {
+		return kafkaapi.ApiKeyEntry{}, err
+	}
+
+	return kafkaapi.ApiKeyEntry{
+		ApiKey:     apiKey,
+		MinVersion: minVersion,
+		MaxVersion: maxVersion,
+	}, nil
+}
---
+++ protocol/decoder/decoder.go
@@ -0,0 +1,139 @@
+package decoder
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+
+	"github.com/codecrafters-io/kafka-tester/offset_buffer"
+	kafkaValue "github.com/codecrafters-io/kafka-tester/protocol/value"
+)
+
+type Decoder struct {
+	buffer *offset_buffer.Buffer
+}
+
+func NewDecoder(rawBytes []byte) *Decoder {
+	return &Decoder{
+		buffer: offset_buffer.NewBuffer(rawBytes),
+	}
+}
+
+func (d *Decoder) RemainingBytesCount() uint64 {
+	return d.buffer.RemainingBytesCount()
+}
+
+// Read functions
+
+func (d *Decoder) ReadInt16() (kafkaValue.Int16, DecoderError) {
+	if d.RemainingBytesCount() < 2 {
+		rem := d.RemainingBytesCount()
+		return kafkaValue.Int16{}, d.wrapError(fmt.Errorf("Expected INT16 length to be 2 bytes, got %d bytes", rem))
+	}
+
+	decodedInteger := kafkaValue.Int16{
+		Value: int16(binary.BigEndian.Uint16(d.buffer.MustReadNBytes(2))),
+	}
+
+	return decodedInteger, nil
+}
+
+func (d *Decoder) ReadInt32() (kafkaValue.Int32, DecoderError) {
+	if d.RemainingBytesCount() < 4 {
+		rem := d.RemainingBytesCount()
+		return kafkaValue.Int32{}, d.wrapError(fmt.Errorf("Expected INT32 length to be 4 bytes, got %d bytes", rem))
+	}
+
+	decodedInteger := kafkaValue.Int32{
+		Value: int32(binary.BigEndian.Uint32(d.buffer.MustReadNBytes(4))),
+	}
+
+	return decodedInteger, nil
+}
+
+func (d *Decoder) ReadUnsignedVarint() (kafkaValue.UnsignedVarint, DecoderError) {
+	decodedInteger, numberOfBytesRead := binary.Uvarint(d.buffer.RemainingBytes())
+
+	// Moves the offset
+	d.buffer.MustReadNBytes(uint64(numberOfBytesRead))
+
+	// binary.Uvarint returns 0 bytes read if buffer is too small
+	if numberOfBytesRead == 0 {
+		return kafkaValue.UnsignedVarint{}, d.wrapError(errors.New("Insufficient bytes to decode UNSIGNED_VARINT"))
+	}
+
+	// binary.Uvarint returns negative if malformed
+	if numberOfBytesRead < 0 {
+		return kafkaValue.UnsignedVarint{}, d.wrapError(errors.New("Malformed UNSIGNED_VARINT encoding"))
+	}
+
+	return kafkaValue.UnsignedVarint{
+		Value: decodedInteger,
+	}, nil
+}
+
+func (d *Decoder) ReadCompactArrayLength() (kafkaValue.CompactArrayLength, DecoderError) {
+	unsignedVarInt, err := d.ReadUnsignedVarint()
+
+	if err != nil {
+		return kafkaValue.CompactArrayLength{}, err
+	}
+
+	return kafkaValue.CompactArrayLength(unsignedVarInt), nil
+}
+
+func (d *Decoder) ConsumeTagBuffer() DecoderError {
+	tagCount, err := d.ReadUnsignedVarint()
+	if err != nil {
+		return err
+	}
+
+	// skip over any tagged fields without deserializing them
+	// as we don't currently support doing anything with them
+	for range tagCount.Value {
+		// ignore tag identifier
+		_, err := d.ReadUnsignedVarint()
+
+		if err != nil {
+			return err
+		}
+
+		// value length
+		length, err := d.ReadUnsignedVarint()
+
+		if err != nil {
+			return err
+		} else if int(length.Value) < 0 {
+			return d.wrapError(errors.New("Expected length of value in tag buffer to be non-negative"))
+		}
+
+		// value
+		if err := d.consumeRawBytes(length.Value); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (d *Decoder) consumeRawBytes(length uint64) DecoderError {
+	if length > uint64(d.RemainingBytesCount()) {
+		return d.wrapError(fmt.Errorf("Expected length to be lesser than remaining bytes (%d), got %d", d.RemainingBytesCount(), length))
+	}
+
+	// Moves the offset
+	d.buffer.MustReadNBytes(length)
+
+	return nil
+}
+
+func (d *Decoder) wrapError(err error) DecoderError {
+	// If we've already wrapped the error, preserve the nested path
+	if decoderError, ok := err.(*decoderErrorImpl); ok {
+		return decoderError
+	}
+
+	return &decoderErrorImpl{
+		message: err.Error(),
+		offset:  int(d.buffer.Offset()),
+	}
+}
