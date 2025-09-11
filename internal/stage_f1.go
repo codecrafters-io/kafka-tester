0c9fe1ce3b7eb3964391faccfe9bef0b1b4aae15
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2025-09-11 12:49:43 +0545

    Use new interfaces in first stage of every extension

--- internal/stage_f1.go
+++ internal/stage_f1.go
@@ -2,56 +2,57 @@ package internal
 
 import (
 	"github.com/codecrafters-io/kafka-tester/internal/kafka_executable"
-	"github.com/codecrafters-io/kafka-tester/internal/legacy_assertions"
-	"github.com/codecrafters-io/kafka-tester/protocol/legacy_builder"
-	"github.com/codecrafters-io/kafka-tester/protocol/legacy_kafka_client"
-	"github.com/codecrafters-io/kafka-tester/protocol/legacy_serializer"
+	"github.com/codecrafters-io/kafka-tester/internal/response_asserter"
+	"github.com/codecrafters-io/kafka-tester/internal/response_assertions"
+	"github.com/codecrafters-io/kafka-tester/internal/response_decoders"
+	"github.com/codecrafters-io/kafka-tester/protocol/builder"
+	"github.com/codecrafters-io/kafka-tester/protocol/kafka_client"
+	"github.com/codecrafters-io/kafka-tester/protocol/kafka_files_handler"
+	"github.com/codecrafters-io/kafka-tester/protocol/kafkaapi"
 	"github.com/codecrafters-io/tester-utils/logger"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
 func testAPIVersionWithFetchKey(stageHarness *test_case_harness.TestCaseHarness) error {
-	b := kafka_executable.NewKafkaExecutable(stageHarness)
-	err := legacy_serializer.GenerateLogDirs(logger.GetQuietLogger(""), false)
-	if err != nil {
+	stageLogger := stageHarness.Logger
+
+	if err := kafka_files_handler.NewFilesHandler(logger.GetQuietLogger("")).GenerateServerConfiguration(); err != nil {
 		return err
 	}
 
-	stageLogger := stageHarness.Logger
+	b := kafka_executable.NewKafkaExecutable(stageHarness)
+
 	if err := b.Run(); err != nil {
 		return err
 	}
 
-	correlationId := getRandomCorrelationId()
+	client := kafka_client.NewClient("localhost:9092")
 
-	client := legacy_kafka_client.NewClient("localhost:9092")
 	if err := client.ConnectWithRetries(b, stageLogger); err != nil {
 		return err
 	}
+
 	defer client.Close()
 
-	request := legacy_builder.NewApiVersionsRequestBuilder().
-		WithCorrelationId(correlationId).
-		Build()
+	correlationId := getRandomCorrelationId()
+	request := builder.NewApiVersionsRequestBuilder().WithCorrelationId(correlationId).Build()
 
 	rawResponse, err := client.SendAndReceive(request, stageLogger)
 	if err != nil {
 		return err
 	}
 
-	actualResponse := legacy_builder.NewApiVersionsResponseBuilder().BuildEmpty()
-	if err := actualResponse.Decode(rawResponse.Payload, stageLogger); err != nil {
-		return err
-	}
+	assertion := response_assertions.NewApiVersionsResponseAssertion().
+		ExpectCorrelationId(correlationId).
+		ExpectErrorCode(0).
+		ExpectApiKeyEntry(18, 0, 4).
+		ExpectApiKeyEntry(1, 0, 16)
 
-	expectedApiVersionResponse := legacy_builder.NewApiVersionsResponseBuilder().
-		AddApiKeyEntry(1, 0, 16).
-		AddApiKeyEntry(18, 0, 4).
-		WithCorrelationId(correlationId).
-		Build()
+	_, err = response_asserter.ResponseAsserter[kafkaapi.ApiVersionsResponse]{
+		DecodeFunc: response_decoders.DecodeApiVersionsResponse,
+		Assertion:  assertion,
+		Logger:     stageLogger,
+	}.DecodeAndAssert(rawResponse.Payload)
 
-	if err = legacy_assertions.NewApiVersionsResponseAssertion(actualResponse, expectedApiVersionResponse).Run(stageLogger); err != nil {
-		return err
-	}
-	return nil
+	return err
 }
