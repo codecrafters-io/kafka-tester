5d71ee8d16ea16ac60d67bb3294b2e237ab6d0bf
Udeshya Dhungana <075bct095.udeshya@pcampus.edu.np>
2026-01-27 14:22:52 +0545

    Add comments about installing kafka in your_program.sh and update fixtures using the 4.0.0 version

--- internal/test_helpers/pass_all/your_program.sh
+++ internal/test_helpers/pass_all/your_program.sh
@@ -2,8 +2,11 @@
 # echo "Starting Kafka using KRaft (Kafka Raft Metadata)"
 # For testing locally, you need to have the latest kafka version in /usr/local/bin/kafka-latest
 # Get it from here: https://github.com/codecrafters-io/build-your-own-kafka/blob/main/kafka_2.13-4.0.0-SNAPSHOT.tgz
-# Extract it and make sure you update the permissions
+# Extract it to /usr/local as kafka-latest and make sure you update the permissions
+# tar -xvf kafka_2.13-4.0.0-SNAPSHOT.tgz
+# mv kafka_2.13-4.0.0-SNAPSHOT.tgz kafka-latest
+# mv kafka-latest /usr/local
 # sudo chown -R user:group ./kafka-latest
 # rm -rf /tmp/kafka-logs /tmp/zookeeper /tmp/kraft-combined-logs
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
+SCRIPT_DIR="$(dirname "$0")"
 /usr/local/kafka-latest/bin/kafka-server-start.sh $@ > /dev/null
